d165c0eb38f1f68d19b74fd3faf001b2871d541f
cgabriel5 <carlosgabriel71@yahoo.com>
2019-01-28 13:38:29 -0800

    refactor: Bash version must to be 4.3+

--- src/scripts/main.sh
+++ src/scripts/main.sh
@@ -1,20 +1,23 @@
-acscript="$HOME/.nodecliac/ac.sh"
-if [[ -f "$acscript" ]]; then
-	# Loop over map definition files to source them.
-	# [https://stackoverflow.com/a/43606356]
-	for filepath in ~/.nodecliac/maps/*; do
-		# Get dir and filename.
+# Bash version must be 4.3+ to register completion scripts to commands.
+if [[ ${BASH_VERSINFO[0]} -ge 4 && ${BASH_VERSINFO[1]} -ge 3 ]]; then
+	acscript="$HOME/.nodecliac/ac.sh"
+	if [[ -f "$acscript" ]]; then
+		# Loop over map definition files to source them.
+		# [https://stackoverflow.com/a/43606356]
 		for filepath in ~/.nodecliac/defs/*; do
-		# Get filename from file path.
-		filename="${filepath##*/}"
-		# Get command name (everything up to first period).
-		command="${filename%%.*}"
+			# Get dir and filename.
+			# dir=${filepath%/*}
+			# Get filename from file path.
+			filename="${filepath##*/}"
+			# Get command name (everything up to first period).
+			command="${filename%%.*}"
 
-		# Only register script to command if command exists in filename.
-		if [[ ! -z "$command" ]]; then
-			# If command exists then register completion script to command.
-			# Note: Command is provided to script as the first parameter.
-			source "$acscript" "${filepath##*/}"
-		fi
-	done
+			# Only register script to command if command exists in filename.
+			if [[ ! -z "$command" ]]; then
+				# If command exists then register completion script to command.
+				# Note: Command is provided to script as the first parameter.
+				source "$acscript" "${command##*/}" "$filename"
+			fi
+		done
+	fi
 fi

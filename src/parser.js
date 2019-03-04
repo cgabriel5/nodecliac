128ba3ba1a8329b3e93e9fac6815ba5475789ac6
cgabriel5 <carlosgabriel71@yahoo.com>
2019-03-03 20:45:28 -0800

    refactor: Dedupe flags

--- src/parser.js
+++ src/parser.js
@@ -656,6 +656,41 @@ module.exports = (contents, source) => {
 		}
 	};
 
+	/**
+	 * Make the provided array unique.
+	 *
+	 * @param  {array} array - The array to clean.
+	 * @param  {boolean} flag_sort - Flag indicating whether the array needs to be sorted.
+	 * @return {array} - The worked on array.
+	 *
+	 * @resource [http://stackoverflow.com/questions/1960473/unique-values-in-an-array/39272981#39272981]
+	 * @ersource [http://stackoverflow.com/questions/1063007/how-to-sort-an-array-of-integers-correctly/21595293#21595293]
+	 */
+	let make_unique = function(array, flag_sort) {
+		// Make array unique.
+		array = array.filter(function(x, i, a_) {
+			return a_.indexOf(x) === i;
+		});
+
+		// Sort the array if flag set.
+		// **Note: does not sort numbers.
+		if (flag_sort) {
+			if (flag_sort === "alpha") {
+				// [https://stackoverflow.com/a/16481400]
+				array = array.sort(function(a, b) {
+					return a.localeCompare(b);
+				});
+			} else if (flag_sort === "number") {
+				array.sort(function(a, b) {
+					return a - b;
+				});
+			}
+		}
+
+		// Return the array.
+		return array;
+	};
+
 	// Exit if file is empty.
 	if (!contents.trim().length) {
 		exit([
@@ -785,18 +820,16 @@ module.exports = (contents, source) => {
 					return prev.concat(curr);
 				});
 
-				// Sort flags is multiple sets exist.
+				// Dedupe and sort flags if multiple sets exist.
 				if (fcount) {
-					flags = flags
-						// Parse on unescaped '|' characters:
-						// [https://stackoverflow.com/a/25895905]
-						// [https://stackoverflow.com/a/12281034]
-						.split(/(?<=[^\\]|^|$)\|/)
-						// [https://stackoverflow.com/a/16481400]
-						.sort(function(a, b) {
-							return a.localeCompare(b);
-						})
-						.join("|");
+					flags = make_unique(
+						flags
+							// Parse on unescaped '|' characters:
+							// [https://stackoverflow.com/a/25895905]
+							// [https://stackoverflow.com/a/12281034]
+							.split(/(?<=[^\\]|^|$)\|/),
+						"alpha"
+					).join("|");
 				}
 			}
 

2a02ada58c70153561db5cb12e16fd6db13f9afd
cgabriel5 <carlosgabriel71@yahoo.com>
2019-03-05 23:04:23 -0800

    refactor: Improve print.js command detection logic
    - Improve command detection logic.
    - Command can be provided in one of two ways: command name only or
    command name with file extension. For example, the following are both
    valid: 'nodecliac' or 'nodecliac.acdef'. Both will work.
    - Add invalid command name checks.

--- src/print.js
+++ src/print.js
@@ -24,9 +24,52 @@ module.exports = args => {
 
 	// If command is supplied then print its acdef/config file contents.
 	if (command) {
+		// Break apart command.
+		let [, commandname = ""] = command.match(/^(.*?)(\.(acdef))?$/);
+		let ext = ".acdef";
+
+		// Exit and give error if a command name not provided.
+		if (!commandname) {
+			exit([
+				`Please provide a command name (i.e. --command=${chalk.bold(
+					"nodecliac.acdef"
+				)}).`
+			]);
+		}
+
+		// Check if command chain contains invalid characters.
+		let r = /[^-._:a-zA-Z0-9\\/]/;
+		if (r.test(commandname)) {
+			// Loop over command chain to highlight invalid character.
+			let chars = [];
+			let invalid_char_count = 0;
+			for (let i = 0, l = commandname.length; i < l; i++) {
+				// Cache current loop item.
+				let char = commandname[i];
+
+				// If an invalid character highlight.
+				if (r.test(char)) {
+					chars.push(chalk.bold.red(char));
+					invalid_char_count++;
+				} else {
+					chars.push(char);
+				}
+			}
+
+			// Plural output character string.
+			let char_string = `character${invalid_char_count > 1 ? "s" : ""}`;
+
+			// Invalid escaped command-flag found.
+			exit([
+				`${chalk.bold(
+					"Invalid:"
+				)} ${char_string} in command: ${chars.join("")}`
+			]);
+		}
+
 		// File paths.
-		let filepath = `${customdir}/defs/${command}.acdef`;
-		let filepathconfig = `${customdir}/defs/.${command}.config.acdef`;
+		let filepath = `${customdir}/defs/${commandname}${ext}`;
+		let filepathconfig = `${customdir}/defs/.${commandname}.config${ext}`;
 
 		// Check if acdef file exists.
 		fe(filepath, (err, exists) => {
@@ -44,7 +87,7 @@ module.exports = args => {
 					}
 
 					// Log file contents.
-					console.log(`[${chalk.bold(`${command}.acdef`)}]\n`);
+					console.log(`[${chalk.bold(`${commandname}${ext}`)}]\n`);
 					console.log(data.toString());
 				});
 
@@ -65,7 +108,9 @@ module.exports = args => {
 
 							// Log file contents.
 							console.log(
-								`[${chalk.bold(`.${command}.config.acdef`)}]\n`
+								`[${chalk.bold(
+									`.${commandname}.config${ext}`
+								)}]\n`
 							);
 							console.log(data.toString());
 						});
@@ -75,7 +120,7 @@ module.exports = args => {
 				// If acdef file does not exist log a message and exit script.
 				exit([
 					`acdef file for command ${chalk.bold(
-						command
+						commandname
 					)} does not exist.`
 				]);
 			}

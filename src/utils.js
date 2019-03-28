c07a4d172d9e56b95c2d0ba909c2144dee975a5e
cgabriel5 <carlosgabriel71@yahoo.com>
2019-03-27 18:07:12 -0700

    chore: Add concat_sets to util.js functions

--- src/utils.js
+++ src/utils.js
@@ -99,7 +99,25 @@ let paths = (function(os, path) {
 	};
 })(os, path);
 
+/**
+ * Concat values from N sets into a main set.
+ *
+ * @param  {set} set - The main set iterable.
+ * @param  {...set} iterables - N amount of sets.
+ * @return {set} - The main set with other merged set values.
+ *
+ * @resource [https://stackoverflow.com/a/41328397]
+ */
+let concat_sets = function(set, ...iterables) {
+	for (let iterable of iterables) {
+		for (let item of iterable) {
+			set.add(item);
+		}
+	}
+};
+
 module.exports = {
+	concat_sets,
 	fileinfo,
 	paths,
 	exit

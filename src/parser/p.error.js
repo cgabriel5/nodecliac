83f1046a7dc3de0bfcc6f8403c496318b773f9c3
cgabriel5 <carlosgabriel71@yahoo.com>
2019-05-16 14:17:34 -0700

    update: Simplify/shorten dupe default command warning message

--- src/parser/p.error.js
+++ src/parser/p.error.js
@@ -89,7 +89,7 @@ module.exports = (...args) => {
 			} missing closing '${h(")", "value")}'.`,
 			10: `Empty flag namespace '${char}'.`,
 			11: `Dupe flag: '${name}'.`,
-			12: `Dupe '${h("default", "keyword")}' for command: '${chain}'.`,
+			12: `Dupe ${h("default", "keyword")} (${chain}).`,
 			13: `Empty '${h(
 				"default",
 				"keyword"
@@ -119,7 +119,7 @@ module.exports = (...args) => {
 			2: `Unexpected ${ctype}: '${char}'.`,
 			3: `Invalid flag option.`,
 			4: `Improperly quoted string.`,
-			5: `Dupe '${h("default", "keyword")}' for command: '${chain}'.`,
+			5: `Dupe ${h("default", "keyword")} (${chain}).`,
 			6: `Empty '${h(
 				"default",
 				"keyword"

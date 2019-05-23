c681c1008de84406f42598e40bfeecafc7624957
cgabriel5 <carlosgabriel71@yahoo.com>
2019-05-23 12:35:35 -0700

    chore: Add yarn files (yarn.acmap and resources/nodecliac/plugins/yarn/)

---
+++ resources/nodecliac/plugins/yarn/main.sh
@@ -0,0 +1,144 @@
+# Parses and extracts data from package.json files.
+#
+# Usage:
+#	__yarn_get_package_fields [-g] [-t FIELDTYPE] <field-key>
+#
+# Options:
+#   -g			  Parse global package.json file, if available
+#   -t FIELDTYPE  The field type being parsed (array|boolean|number|object|string) [default: object]
+#
+# Notes:
+#	If FIELDTYPE is object, then the object keys are returned.
+#	If FIELDTYPE is array, boolean, number, or string, then the field values are returned.
+#	<field-key> must be a first-level field in the json file.
+#
+# Resource:
+#    Code lifted and modified from dsifford's yarn-completion [https://github.com/dsifford/yarn-completion].
+##
+__yarn_get_package_fields() {
+	declare cwd=$PWD field_type=object field_key opt package_dot_json OPTIND OPTARG
+
+	# '-n' documentation: [https://likegeeks.com/sed-linux/]
+	while [[ -n $cwd ]]; do
+		if [[ -f "$cwd/package.json" ]]; then
+			package_dot_json="$cwd/package.json"
+			break
+		fi
+		cwd="${cwd%/*}"
+	done
+
+	while getopts ":gt:" opt; do
+		case $opt in
+			g)
+				if [[ -f $HOME/.config/yarn/global/package.json ]]; then
+					package_dot_json="$HOME/.config/yarn/global/package.json"
+				elif [[ -f $HOME/.local/share/yarn/global/package.json ]]; then
+					package_dot_json="$HOME/.local/share/yarn/global/package.json"
+				elif [[ -f $HOME/.yarn/global/package.json ]]; then
+					package_dot_json="$HOME/.yarn/global/package.json"
+				else
+					package_dot_json=""
+				fi
+				;;
+			t)
+				case "$OPTARG" in
+					array | boolean | number | object | string)
+						field_type="$OPTARG"
+						;;
+				esac
+				;;
+			*) ;;
+
+		esac
+	done
+	# [https://unix.stackexchange.com/a/214151]
+	shift $((OPTIND - 1))
+
+	field_key='"'$1'"'
+
+	[[ ! -f $package_dot_json || ! $field_key ]] && return
+
+	# Generate the primitive (boolean, number, string) RegExp patterns.
+	function __ptn_prim() {
+		# Vars.
+		local pattern mpattern
+
+		# RegExp escaping documentation:
+		# [https://unix.stackexchange.com/questions/263668/sed-capture-groups-not-working#comment860675_263675]
+		# [https://stackoverflow.com/a/11651184]
+		# [https://stackoverflow.com/a/2777621]
+		# [https://superuser.com/a/112000]
+
+		# Lookup data type's patterns.
+		case "$1" in
+			boolean) pattern='\(true\|false\)' ;;
+			number)  pattern='"\([\.0-9]*\)"'  ;;
+			string)  pattern='"\(.*\)"'        ;;
+		esac
+
+		# Boolean, string, number general pattern.
+		mpattern="[[:space:]]*$field_key:[[:space:]]*XX.*"
+		# [https://stackoverflow.com/a/13210909]
+		echo "s/${mpattern/XX/$pattern}/\1/p"
+	}
+
+	# Generate the object (object, array) RegExp patterns.
+	function __ptn_objt() {
+		# Vars.
+		local pattern1 pattern2
+
+		# Lookup data type's patterns.
+		case "$1" in
+			object) pattern1='{';  pattern2='}' ;;
+			array)  pattern1='\['; pattern2=']' ;;
+		esac
+
+		# Boolean, string, number general pattern.
+		echo '/'"$field_key"':[[:space:]]*'"$pattern1"'/,/^[[:space:]]*'"$pattern2"'/{
+			# exclude start and end patterns
+			//!{
+				# extract the text between the first pair of double quotes
+				s/^[[:space:]]*"\([^"]*\).*/\1/p
+			}
+		}'
+	}
+
+	# Generate RegExp pattern to extract needed data from package.json.
+	case "$field_type" in
+		object)  pattern="`__ptn_objt object`"  ;;
+		array)   pattern="`__ptn_objt array`"   ;;
+		boolean) pattern="`__ptn_prim boolean`" ;;
+		number)  pattern="`__ptn_prim number`"  ;;
+		string)  pattern="`__ptn_prim string`"  ;;
+	esac
+
+	# Finally, extract package.json data.
+	sed -n "$pattern"  "$package_dot_json"
+}
+
+# Depending on provided action run appropriate logic...
+
+case "$1" in
+	remove|outdated)
+		# Get (dev)dependencies.
+		dev=`__yarn_get_package_fields dependencies`
+		devdep=`__yarn_get_package_fields devDependencies`
+
+		# Return (dev)dependencies values to auto-complete.
+		echo -e "$dev\n$devdep"
+	;;
+	run)
+		# Get script names.
+		scripts=`__yarn_get_package_fields scripts`
+
+		# Run ac perl script to get completions.
+		prune_args_script=~/.nodecliac/plugins/yarn/prune_args.pl
+		# Run completion script if it exists.
+		if [[ -f "$prune_args_script" ]]; then
+			output=`"$prune_args_script" "$scripts"`
+
+			# Return script names.
+			echo -e "\n$output"
+		fi
+	;;
+esac
---
+++ resources/nodecliac/plugins/yarn/prune_args.pl
@@ -0,0 +1,65 @@
+#!/usr/bin/perl
+
+# [https://stackoverflow.com/questions/8023959/why-use-strict-and-warnings]
+# [http://perldoc.perl.org/functions/use.html]
+# use strict;
+# use warnings;
+# use diagnostics;
+
+# Get environment variables.
+my $last = $ENV{'NODECLIAC_LAST'};
+my $lchar = $ENV{'NODECLIAC_LAST_CHAR'};
+my $nchar = $ENV{'NODECLIAC_NEXT_CHAR'};
+
+# Get environment variable containing used default positional arguments.
+my $used = $ENV{'NODECLIAC_USED_DEFAULT_POSITIONAL_ARGS'};
+# Trim string.
+$used =~ s/^\s+|\s+$//g;
+# Split string into individual items.
+my @used_args = split(/\n/, $used);
+
+# Get passed in argument (i.e. script names, (dev)dependencies).
+my $args = $ARGV[0];
+# Trim string.
+$args =~ s/^\s+|\s+$//g;
+# Split string into individual items.
+my @arguments = split(/\n/, $args);
+
+# Array will store unused items.
+my @cleaned_args = ();
+
+# Remove already used items.
+foreach my $arg (@arguments) {
+	# If the argument is in the used_args array then skip it.
+	# [https://stackoverflow.com/a/20570606]
+	if (!grep(/^$arg$/, @used_args)) {
+		# If a last character exists then we need only return completion
+		# items that start with the last word.
+		if ($lchar) {
+			if (index($arg, $last) == 0) { push(@cleaned_args, $arg);}
+		}
+		# ..else return all completion items.
+		else {
+			push(@cleaned_args, $arg);
+		}
+	}
+}
+
+# Return unused items string.
+my $final_args = join("\n", @cleaned_args);
+
+# If no completion items exist do some final checks...
+if (
+	# If no auto-completion arguments exist...
+	!$final_args &&
+	# ...and if there is no next char or the next char is a space...
+	(!$nchar || $nchar eq " ") &&
+	# ...and if the last item is in the provided arguments array...
+	grep(/^$last$/, @arguments)
+) {
+	# Add the currently last item to the completion items.
+	print "\n$last";
+}
+
+# Return unused items string.
+print "$final_args";

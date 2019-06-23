0f0a81181f915b0dd4782125df075854dd1181b7
cgabriel5 <carlosgabriel71@yahoo.com>
2019-06-22 23:16:36 -0700

    refactor: Remove __fallback_cmd_string function, inline code
    - Remove __fallback_cmd_string function to remove redundant chains
    loop.

--- src/scripts/ac.pl
+++ src/scripts/ac.pl
@@ -309,76 +309,6 @@ sub __validate {
 	return $arg;
 }
 
-# Run command-string fallback logic.
-#
-# @param {string} 1) - The command-string's keyword (default/always).
-# @param {array} 2) - The command chains array reference.
-# @return {null} - Nothing is returned.
-sub __fallback_cmd_string {
-	# Get arguments.
-	my ($keyword, $chains_ref) = @_;
-	# Dereference and use array.
-	my @chains = @{ $chains_ref };
-	# Get chains count.
-	my $chains_count = __len(\@chains);
-
-	# If no chains exists then populate array to always try and run the
-	# main commands command-string.
-	if (!$chains_count && $keyword eq "default") {
-		# Add empty string so simply run loop logic.
-		push(@chains, "");
-		$chains_count = 1;
-	}
-
-	# Loop over chains and run first found command-string.
-	for (my $i = 0; $i < $chains_count; $i++) {
-		# Cache chain.
-		my $chain = $chains[$i];
-
-		# Get command-string, parse it, then run it...
-		my $pattern = '^' . quotemeta($chain) . " $keyword" . '[ \t]{1,}(.*?)$';
-		if ($acdef =~ /$pattern/m) {
-			# Store matched RegExp pattern value.
-			my $value = $1;
-			# If match exists...
-			if ($value) {
-				# Check if it is a command-string.
-				my $pattern = '^\$\((.*?)\)$';
-				if ($value =~ /$pattern/m) {
-					# Get the command-flag.
-					if ($1) {
-						# Parse user provided command-flag command.
-						__execute_command($1);
-					}
-				}
-				# Else it is a static non command-string value.
-				else {
-					if ($last) {
-						# When last word is present only
-						# add words that start with last
-						# word.
-
-						# Since we are completing a command we only
-						# want words that start with the current
-						# command we are trying to complete.
-						my $pattern = '^' . quotemeta($last);
-						if ($value =~ /$pattern/) {
-							# Finally, add to flags array.
-							push(@completions, $value);
-						}
-					} else {
-						# Finally, add to flags array.
-						push(@completions, $value);
-					}
-				}
-			}
-
-			# Stop loop once a command-string is found and ran.
-			last;
-		}
-	}
-}
-
 # Parse and run command-flag (flag) or default command chain command
 #     (commandchain).
 #
@@ -1582,31 +1512,69 @@ sub __lookup {
 			}
 		}
 
-		# Copy commandchain string.
-		my $copy_commandchain = $commandchain;
-		# Create chains array.
-		my @chains = ($commandchain); # Set command chain as the first chain.
-		# Loop over command chains to build individual chain levels.
-		while ($copy_commandchain) {
-			# Remove last command chain from overall command chain.
-			$copy_commandchain = $copy_commandchain =~ s/\.((?:\\\.)|[^\.])+$//r; # ((?:\\\.)|[^\.]*?)*$
-			# Store chain level in chains array.
-			if ($copy_commandchain) { push(@chains, $copy_commandchain); }
-		}
-
 		# If no completions exist run default command if it exists.
 		if (!__len(\@completions)) {
-			# Run default command-string.
-			__fallback_cmd_string("default", \@chains);
-		}
+			# Copy commandchain string.
+			my $copy_commandchain = $commandchain;
+			# Keyword to look for.
+			my $keyword = "default";
 
-		# # Note: 'always' keyword has quirks so comment out for now.
-		# # Note: When running the 'always' fallback should the current command
-		# # chain's fallback be looked and run or should the command chain also
-		# # be broken up into levels and run the first available fallback always
-		# # command-string?
-		# my @chains = ($commandchain);
-		# __fallback_cmd_string("always", \@chains);
+			# Loop over command chains to build individual chain levels.
+			while ($copy_commandchain) {
+				# Get command-string, parse it, then run it...
+				my $pattern = '^' . quotemeta($copy_commandchain) . " $keyword" . '[ \t]{1,}(.*?)$';
+				if ($acdef =~ /$pattern/m) {
+					# Store matched RegExp pattern value.
+					my $value = $1;
+					# If match exists...
+					if ($value) {
+						# Check if it is a command-string.
+						my $pattern = '^\$\((.*?)\)$';
+						if ($value =~ /$pattern/m) {
+							# Get the command-flag.
+							if ($1) {
+								# Parse user provided command-flag command.
+								__execute_command($1);
+							}
+						}
+						# Else it is a static non command-string value.
+						else {
+							if ($last) {
+								# When last word is present only
+								# add words that start with last
+								# word.
+
+								# Since we are completing a command we only
+								# want words that start with the current
+								# command we are trying to complete.
+								my $pattern = '^' . quotemeta($last);
+								if ($value =~ /$pattern/) {
+									# Finally, add to flags array.
+									push(@completions, $value);
+								}
+							} else {
+								# Finally, add to flags array.
+								push(@completions, $value);
+							}
+						}
+					}
+
+					# Stop loop once a command-string is found and ran.
+					last;
+				}
+			}
+
+			# Remove last command chain from overall command chain.
+			$copy_commandchain = $copy_commandchain =~ s/\.((?:\\\.)|[^\.])+$//r; # ((?:\\\.)|[^\.]*?)*$
+
+			# # Note: 'always' keyword has quirks so comment out for now.
+			# # Note: When running the 'always' fallback should the current command
+			# # chain's fallback be looked and run or should the command chain also
+			# # be broken up into levels and run the first available fallback always
+			# # command-string?
+			# my @chains = ($commandchain);
+			# __fallback_cmd_string("always", \@chains);
+		}
 	}
 }
 

d751887ebd0c92236119d09d92070fa63a14dc1e
cgabriel5 <carlosgabriel71@yahoo.com>
2019-07-03 11:12:53 -0700

    docs: Document prehook passed in available arguments

--- resources/nodecliac/yarn/hooks/prehook.sh
+++ resources/nodecliac/yarn/hooks/prehook.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 
+# Prehook script gets provided the following arguments:
+# $1 => $cline        # Original (complete) CLI input.
+# $2 => $cpoint       # Caret index when [tab] key was pressed.
+# $3 => $maincommand  # The acdef definitions file.
+# $4 => $acdef        # The command name from sourced passed-in argument.
+
+# The script will run the needed Perl scripts and return the modified output.
+
 # Run input hook script.
 output=`"$HOME/.nodecliac/registry/yarn/hooks/input.pl" "$1"`
 # Reset variable if output exists.

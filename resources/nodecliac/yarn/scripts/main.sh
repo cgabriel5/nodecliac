79e7934931b7082293ce189b4c2423f1b2d78c65
cgabriel5 <carlosgabriel71@yahoo.com>
2019-07-05 17:48:06 -0700

    refactor: Use Single Perl command call to get (dev)dependencies entries

--- resources/nodecliac/yarn/scripts/main.sh
+++ resources/nodecliac/yarn/scripts/main.sh
@@ -103,7 +103,8 @@ function __yarn_get_package_fields() {
 	# args=`LC_ALL=C perl -0777 -ne 'print "$2" while /"(dependencies|devDependencies)"\s*:\s*{([\s\S]*?)}(,|$)/g' package.json | LC_ALL=C perl -ne 'print "$1\n" while /"([a-zA-Z][-a-zA-Z0-9]*)"\s*:\s*"/g'`
 	# args=`LC_ALL=C perl -0777 -ne 'print "$2" while /"(scripts)"\s*:\s*{([\s\S]*?)}(,|$)/g' package.json | LC_ALL=C perl -ne 'print "$1\n" while /"([a-zA-Z][-a-zA-Z0-9]*)"\s*:\s*"/g'`
 	# Perl only solution:
-	LC_ALL=C perl -0777 -ne "print \"\$2\" while /\"($1)\"\\s*:\\s*{([\\s\\S]*?)}(,|$)/g" "$package_dot_json" | LC_ALL=C perl -ne "print \"\$1\\n\" while /\"([a-zA-Z][-a-zA-Z0-9]*)\"\\s*:\\s*\"/g" 2> /dev/null
+	# LC_ALL=C perl -0777 -ne "print \"\$2\" while /\"($1)\"\\s*:\\s*{([\\s\\S]*?)}(,|$)/g" "$package_dot_json" | LC_ALL=C perl -ne "print \"\$1\\n\" while /\"([a-zA-Z][-a-zA-Z0-9]*)\"\\s*:\\s*\"/g" 2> /dev/null
+	LC_ALL=C perl -0777 -ne "while (/\"($1)\"\\s*:\\s*{([\\s\\S]*?)}(,|$)/g) { my @matches = \$2 =~ /\"([^\"]*)\"\\s*:\\s*\"/g; foreach (@matches) { print \"\$_\\n\"; }}" <<< `<"$package_dot_json"` 2> /dev/null
 }
 
 # Perl script path.

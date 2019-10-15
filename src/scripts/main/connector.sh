81f54a5760536e22b428a04a75ddd8d3a4f5ec7f
cgabriel5 <carlosgabriel71@yahoo.com>
2019-10-14 20:04:44 -0700

    update: Collapse lines to a single line

--- src/scripts/main/connector.sh
+++ src/scripts/main/connector.sh
@@ -1107,9 +1107,7 @@ if [[ -n "$1" ]] && type complete &>/dev/null; then
 		# Default to Nim ac script for completion logic.
 		acpl_script=~/.nodecliac/src/bin/ac."$(e=$(uname);e=${e,,};echo ${e/darwin/macosx})"
 		# If Nim script does not exist fallback to Perl script.
-		if [[ ! -f  "$acpl_script" ]]; then
-			acpl_script=~/.nodecliac/src/ac/ac.pl
-		fi
+		if [[ ! -f  "$acpl_script" ]]; then acpl_script=~/.nodecliac/src/ac/ac.pl; fi
 
 		# Run completion script if it exists.
 		if [[ -f "$acpl_script" ]]; then

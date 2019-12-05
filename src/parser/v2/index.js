0e9484554ef7a92feae94f26af5467897dcf8d32
cgabriel5 <carlosgabriel71@yahoo.com>
2019-12-05 12:13:28 -0800

    refactor: Further cleanup v2/index.js (parser entry point)

--- src/parser/v2/index.js
+++ src/parser/v2/index.js
@@ -1,50 +1,30 @@
 "use strict";
 
 module.exports = (string, commandname, source, formatting, ...args) => {
-	// Vars - destructure remaining arguments.
-	let [highlight, trace /* nowarn*/, , stripcomments, test] = args;
-
-	// Vars - timers.
-	let stime = process.hrtime(); // Store start time tuple array.
-	// Vars - loop.
+	const [highlight, trace /* nowarn*/, , stripcomments, test] = args;
+	const stime = process.hrtime(); // Start time.
 	let first_non_whitespace_char = "";
 	let line_type;
 
-	// Loop global state variables.
+	// Loop state object.
 	const STATE = {
-		// Loop variables.
 		line: 1,
 		column: 0,
 		i: 0,
 		l: string.length,
 		string,
 		specificity: 0, // Default to allow anything initially.
-
-		// Track command-chain and flag scopes.
-		scopes: { command: null, flag: null },
+		scopes: { command: null, flag: null }, // Track command/flag scopes.
 
 		// Parsing lookup tables.
-		tables: {
-			variables: {}, // Contain variables: name:value.
-			linestarts: {}, // Contain line index start points.
-			tree: { nodes: [] } // Collection of parsed NODES.
-		},
+		tables: { variables: {}, linestarts: {}, tree: { nodes: [] } },
 
-		// Add arguments/parameters for easy access across parsers.
-		args: {
-			// commandname, source, nowarn,
-			formatting,
-			highlight,
-			trace,
-			stripcomments,
-			test
-		},
+		// Arguments/parameters for quick access across parsers.
+		args: { formatting, highlight, trace, stripcomments, test },
 
-		// Attach utilities (helper functions/constants) for quick access.
+		// Utilities (helper functions/constants) for quick access.
 		utils: {
-			constants: {
-				regexp: require("./helpers/patterns.js")
-			},
+			constants: { regexp: require("./helpers/patterns.js") },
 			functions: {
 				tree: { add: require("./helpers/tree-add.js") },
 				loop: {
@@ -57,12 +37,22 @@ module.exports = (string, commandname, source, formatting, ...args) => {
 		}
 	};
 
+	// Mmain loop helper functions/constants.
+	const linetype = require("./helpers/line_type.js");
+	const formatter = require("./helpers/formatter.js");
+	const indentation = require("./helpers/indentation.js");
+	const specificity = require("./helpers/specificity.js");
+	const bracechecks = require("./helpers/brace-checks.js");
+	const { r_start_line_char } = STATE.utils.constants.regexp;
+	const { issue } = STATE.utils.functions.loop;
+	const { linestarts } = STATE.tables;
+
 	// Loop over acdef file contents to parse.
 	for (; STATE.i < STATE.l; STATE.i++) {
 		let char = string.charAt(STATE.i); // Cache current loop char.
 		let nchar = string.charAt(STATE.i + 1);
 
-		// Handle new lines.
+		// Handle newlines.
 		if (char === "\n") {
 			require(`./parsers/newline.js`)(STATE); // Run newline parser.
 
@@ -76,34 +66,24 @@ module.exports = (string, commandname, source, formatting, ...args) => {
 		STATE.column++; // Increment column position.
 
 		// Store line start points.
-		if (!STATE.tables.linestarts[STATE.line]) {
-			STATE.tables.linestarts[STATE.line] = STATE.i;
-		}
+		if (!linestarts[STATE.line]) linestarts[STATE.line] = STATE.i;
 
 		// Find first non-whitespace character of line.
 		if (!first_non_whitespace_char && !/[ \t]/.test(char)) {
 			first_non_whitespace_char = char; // Set flag.
 
-			// Error if start-of-line character is not allowed.
-			if (!STATE.utils.constants.regexp.r_start_line_char.test(char)) {
-				// Invalid start-of-line char.
-				STATE.utils.functions.loop.issue.error(STATE, 10);
-			}
+			// Error if sol char is not allowed.
+			if (!r_start_line_char.test(char)) issue.error(STATE, 10);
 
 			// Note: Since current sol char has already been iterated over,
 			// rollback column to let parser start on sol char.
 			STATE.column--;
 
-			// Determine line's type.
-			line_type = require("./helpers/line_type.js")(STATE, char, nchar);
-			// Break from loop if terminator char found.
-			if (line_type === "terminator") break;
+			line_type = linetype(STATE, char, nchar); // Get line's type.
+			if (line_type === "terminator") break; // End on terminator char.
 
-			// Check line indentation.
-			require("./helpers/indentation.js")(STATE, line_type);
-
-			// Check line specificity hierarchy.
-			require("./helpers/specificity.js")(STATE, line_type);
+			indentation(STATE, line_type); // Validate line indentation.
+			specificity(STATE, line_type); // Validate line specificity.
 
 			let parser = `${line_type}.js`;
 			require("./helpers/trace.js")(STATE, parser); // Trace parser.
@@ -112,18 +92,11 @@ module.exports = (string, commandname, source, formatting, ...args) => {
 	}
 
 	// If command-chain scope exists post-parsing then it was never closed.
-	require("./helpers/brace-checks.js")(STATE, null, "post-standing-scope");
+	bracechecks(STATE, null, "post-standing-scope");
 
-	if (formatting) {
-		// Return prettified ACMAP.
-		return {
-			time: process.hrtime(stime), // Return end time tuple array.
-			formatted: require("./helpers/formatter.js")(STATE)
-		};
-	} else {
-		let res = require("./helpers/acdef.js")(STATE, commandname);
-		res.time = process.hrtime(stime); // Return end time tuple array.
-
-		return res; // Return acdef, config, etc.
-	}
+	let res = {};
+	if (formatting) res.formatted = formatter(STATE);
+	else res = require("./helpers/acdef.js")(STATE, commandname);
+	res.time = process.hrtime(stime); // Attach end time.
+	return res; // Return acdef, config, etc.
 };

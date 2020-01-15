77a4004194895c5c348b2c49768fae79e3c8e165
cgabriel5 <carlosgabriel71@yahoo.com>
2020-01-14 20:02:20 -0800

    refactor: Re-write registry command to match its JavaScript counterpart

--- src/scripts/bin/aconly.sh
+++ src/scripts/bin/aconly.sh
@@ -2,6 +2,22 @@
 
 if [[ "$#" -eq 0 ]]; then exit; fi # Exit if no arguments provided.
 
+# Get platform name.
+#
+# @return {string} - User's platform.
+#
+# @resource [https://stackoverflow.com/a/18434831]
+function __platform() {
+	case "$OSTYPE" in
+		solaris*) echo "solaris" ;;
+		darwin*)  echo "macosx" ;;
+		linux*)   echo "linux" ;;
+		bsd*)     echo "bsd" ;;
+		msys*)    echo "windows" ;;
+		*)        echo "unknown" ;;
+	esac
+}
+
 # CLI arg flags.
 rcfilepath=""
 prcommand=""
@@ -14,6 +30,10 @@ command=""
 # [http://tldp.org/LDP/Bash-Beginners-Guide/html/sect_09_07.html]
 params=""
 paramsargs=()
+
+# Paths.
+registrypath=~/.nodecliac/registry
+
 while (( "$#" )); do
  case "$1" in
   	# Custom `print` command flags.
@@ -94,25 +114,105 @@ case "$command" in
 
 	registry)
 
-		count=0; filepaths=""; output=""
 		# Build acdef file paths string. [https://unix.stackexchange.com/a/96904]
 		# for f in ~/.nodecliac/registry/*/*.acdef ~/.nodecliac/registry/*/.*.acdef; do
-		for f in ~/.nodecliac/registry/*/*.acdef; do
-			filepaths="$filepaths$f\n"
+		# for f in ~/.nodecliac/registry/*/*.acdef; do
+
+		# Count items in directory: [https://stackoverflow.com/a/33891876]
+		count=$(ls 2>/dev/null -Ubad1 -- ~/.nodecliac/registry/* | wc -l)
+		if [[ $count -lt 0 ]]; then ((count=count-1)); fi # Account for 0 base index.
+		counter=0
+
+		echo -e "\033[1;30m$registrypath\033[0m ($count)" # Print header.
+
+		# Exit if directory is empty.
+		if [[ "$count" == "0" ]]; then exit; fi
+
+		for f in ~/.nodecliac/registry/*; do
 			filename=$(basename "$f")
-			maincommand="${filename%%.*}"
 			# dirname=$(dirname "$f")
+			# command="${filename%%.*}"
+			command=$(basename "$f")
 
-			if [[ -e ~/.nodecliac/registry/"$maincommand/.$maincommand.config.acdef" ]]; then
-				output+=" ─ \033[1;34m$maincommand\033[0m*\n"
-			else output+=" ─ $maincommand\n"; fi
+			# Build .acdef file paths.
+			filename="$command.acdef"
+			configfilename=".$command.config.acdef"
+			acdefpath="$registrypath/$command/$filename"
+			configpath="$registrypath/$command/$configfilename"
 
-			((count=count+1)) # Increment counter.
+			isdir=0
+			hasacdefs=0
+			issymlink=0
+			issymlinkdir=0
+			realpath=""
+			issymlink_valid=0
+			check=0
+
+			if [[ -f "$acdefpath" ]]; then check=1; fi # Check for .acdef.
+			if [[ -f "$configpath" && "$check" == 1 ]]; then hasacdefs=1; fi # Check for config file.
+
+			# If files exists check whether it's a symlink.
+			pkgpath="$registrypath/$command"
+			if [[ -d "$pkgpath" && ! -L "$pkgpath" ]]; then isdir=1; fi
+
+			if [[ -L "$pkgpath" ]]; then
+				issymlink=1
+
+				# Resolve symlink: [https://stackoverflow.com/a/42918]
+				# Using Python: [https://apple.stackexchange.com/a/4822]
+				if [[ "$(__platform)" == "macosx" ]]; then
+					resolved_path=$(readlink "$pkgpath")
+				else
+					resolved_path=$(readlink -f "$pkgpath")
+				fi
+				realpath="$resolved_path"
+
+				if [[ -d "$resolved_path" ]]; then issymlinkdir=1; isdir=1; fi
+
+				# Confirm symlink directory contain needed .acdefs.
+				sympath="$resolved_path/$command/$filename"
+				sympathconf="$resolved_path/$command/$configfilename"
+
+				check=0
+				if [[ -f "$sympath" ]]; then check=1; fi # Check for .acdef.
+				if [[ -f "$sympathconf" && "$check" == 1 ]]; then issymlink_valid=1; fi # Check for config file.
+			fi
+
+				# Remove user name from path: [https://stackoverflow.com/a/22261454]
+				re="^$HOME/(.*)"
+				[[ "$realpath" =~ $re ]]
+				realpath="~/${BASH_REMATCH[1]}"
+
+				bcommand="\033[1;34m$command\033[0m"
+				ccommand="\033[1;36m$command\033[0m"
+				rcommand="\033[1;31m$command\033[0m"
+
+				# Row declaration.
+				decor="├── "; if [[ "$counter" == "$count" ]]; then decor="└── "; fi
+
+				if [[ "$issymlink" == 0 ]]; then
+					if [[ "$isdir" == 1 ]]; then
+						dcommand=$([ "$hasacdefs" == 1 ] && echo "$bcommand" || echo "$rcommand")
+						echo -e "$decor$dcommand/"
+					else
+						echo -e "$decor$rcommand"
+					fi
+				else
+					if [[ "$issymlinkdir" == 1 ]]; then
+						color=$([ "$issymlink_valid" == 1 ] && echo "\033[1;34m" || echo "\033[1;31m")
+						linkdir="$color$realpath\033[0m"
+						echo -e "$decor$ccommand -> $linkdir/"
+					else
+						echo -e "$decor$ccommand -> $realpath"
+					fi
+				fi
+
+				((counter=counter+1)) # Increment counter.
+
 		done
 
-		header=" \033[1m.acdef files: ($count)\033[0m\n"
-		# Remove trailing newline: [https://unix.stackexchange.com/a/140738]
-		echo -e "$(echo -e "$header$output" | perl -0 -pe 's/\n\Z//')"
+		# # Remove trailing newline: [https://unix.stackexchange.com/a/140738]
+		# echo -e "$(echo -e "$header$output" | perl -0 -pe 's/\n\Z//')"
 
 		;;
 

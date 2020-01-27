8ba5278afae6389a450f79cb2e7710f79d38a315
cgabriel5 <carlosgabriel71@yahoo.com>
2020-01-27 15:32:50 -0800

    update: Rename 'STATE' variable to 'S' (parser/index.js)

--- src/parser/index.js
+++ src/parser/index.js
@@ -10,50 +10,50 @@ const bracechecks = require("./helpers/brace-checks.js");
 const { r_sol_char, r_whitespace } = require("./helpers/patterns.js");
 
 module.exports = (text, commandname, source, fmt, trace, igc, test) => {
-	const STATE = state(text, source, fmt, trace, igc, test);
-	const { linestarts } = STATE.tables;
+	const S = state(text, source, fmt, trace, igc, test);
+	const { linestarts } = S.tables;
 	const stime = process.hrtime(); // Start time.
 	let line_type;
 
 	// Loop over acdef file contents to parse.
-	for (; STATE.i < STATE.l; STATE.i++) {
-		let char = text.charAt(STATE.i);
-		let nchar = text.charAt(STATE.i + 1);
+	for (; S.i < S.l; S.i++) {
+		let char = text.charAt(S.i);
+		let nchar = text.charAt(S.i + 1);
 
 		// Handle newlines.
-		if (char === "\n") p_newline(STATE);
+		if (char === "\n") p_newline(S);
 		// All other characters.
 		else {
-			STATE.column++;
+			S.column++;
 
 			// Store line start points.
-			if (!linestarts[STATE.line]) linestarts[STATE.line] = STATE.i;
+			if (!linestarts[S.line]) linestarts[S.line] = S.i;
 
 			// Find first non-whitespace character of line.
-			if (!STATE.sol_char && !r_whitespace.test(char)) {
-				STATE.sol_char = char; // Set char.
+			if (!S.sol_char && !r_whitespace.test(char)) {
+				S.sol_char = char; // Set char.
 
 				// Error if sol char is not allowed.
-				if (!r_sol_char.test(char)) error(STATE, 10, __filename);
+				if (!r_sol_char.test(char)) error(S, 10, __filename);
 
-				line_type = linetype(STATE, char, nchar); // Get line's type.
+				line_type = linetype(S, char, nchar); // Get line's type.
 				if (line_type === "terminator") break; // End on terminator char.
 
-				specificity(STATE, line_type); // Validate line specificity.
+				specificity(S, line_type); // Validate line specificity.
 
-				STATE.column--; // Rollback column to start parser on sol char.
-				require("./helpers/trace.js")(STATE, line_type); // Trace parser.
-				require(`./parsers/${line_type}.js`)(STATE); // Run parser.
+				S.column--; // Rollback column to start parser on sol char.
+				require("./helpers/trace.js")(S, line_type); // Trace parser.
+				require(`./parsers/${line_type}.js`)(S); // Run parser.
 			}
 		}
 	}
 
 	// If command-chain scope exists post-parsing then it was never closed.
-	bracechecks(STATE, null, "post-standing-scope");
+	bracechecks(S, null, "post-standing-scope");
 
 	let res = {};
-	if (fmt) res.formatted = formatter(STATE);
-	else res = require("./helpers/acdef.js")(STATE, commandname);
+	if (fmt) res.formatted = formatter(S);
+	else res = require("./helpers/acdef.js")(S, commandname);
 	res.time = process.hrtime(stime); // Attach end time.
 	return res; // Return acdef, config, etc.
 };

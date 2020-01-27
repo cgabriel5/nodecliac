a0d9fbc1e822124b0a51c89009ed4d6e94144703
cgabriel5 <carlosgabriel71@yahoo.com>
2020-01-27 12:04:39 -0800

    refactor: Rename issue.js to error.js and simplify file
    - Remove 'caller_filename' function. Just provide parser name manually.

---
+++ src/parser/v2/helpers/error.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const path = require("path");
+const chalk = require("chalk");
+
+// Error lookup table.
+let errors = {
+	"*": {
+		// Universal errors.
+		error: { 0: "Unexpected character (syntax error)." },
+		warning: {}
+	},
+	index: {
+		10: "Illegal start-of-line character.",
+		12: "Check line specificity order."
+	},
+	command: {
+		10: "Illegal escape sequence."
+	},
+	comment: {},
+	flag: {
+		10: "Cannot declare flag out of scope.",
+		11: "Cannot declare flag within flag scope."
+	},
+	option: {},
+	variable: {},
+	setting: {},
+	"close-brace": {},
+	"brace-checks": {
+		10: "Cannot declare command within command scope.",
+		11: "Cannot close an unopened command scope.",
+		12: "Unclosed scope.",
+		13: "Cannot declare flag option out of scope."
+	},
+	"template-string": {},
+	"validate-value": {
+		10: "Improperly quoted string.",
+		11: "String cannot be empty.",
+		12: "Undefined variable.",
+		13: "Illegal command-flag syntax.",
+		14: "Useless comma delimiter.",
+		15: "Illegal list syntax."
+	}
+};
+
+/**
+ * Give error (kills process).
+ *
+ * @param  {object} STATE - Parsing STATE object.
+ * @param  {number} code - Error code.
+ * @param  {string} parserfile - Path of file issuing error.
+ * @return {undefined} - Nothing is returned.
+ */
+module.exports = (STATE, parserfile, code) => {
+	let { line, column } = STATE;
+	let { source } = STATE.args;
+	let parser = path.basename(parserfile).replace(/\.js$/, "");
+	let error;
+
+	// Get error (use default if code doesn't exist).
+	if (!code) {
+		code = 0; // Reset to default error.
+		error = errors["*"].error[code];
+	} else error = errors[parser][code];
+
+	let pos = chalk.bold.red(`${line}:${column}`);
+	let einfo = "[" + chalk.red(`err`) + ` ${parser},${code}` + "]";
+
+	// Truncate source file path if too large.
+	let dirs = source.split(path.sep);
+	if (dirs.length >= 5) {
+		dirs = dirs.slice(dirs.length - 3);
+		source = "..." + dirs.join(path.sep);
+	}
+
+	let filename = chalk.bold(path.basename(source));
+	let dirname = path.dirname(source);
+
+	console.log(`${einfo} ${dirname}/${filename}:${pos} — ${error}`);
+	process.exit();
+};

6e292a094ecb08b2aa77aa10ae12ee5cffa30d3b
cgabriel5 <carlosgabriel71@yahoo.com>
2020-01-28 12:23:32 -0800

    update: Cleanup utils/exit.js

--- src/utils/exit.js
+++ src/utils/exit.js
@@ -9,21 +9,13 @@ const log = require("fancy-log");
  * @return {undefined} - Nothing.
  */
 let exit = (messages, stop, normal_log) => {
-	// Log all provided messages.
 	for (let i = 0, l = messages.length; i < l; i++) {
-		// Cache current loop item.
 		let message = messages[i];
-
-		if (normal_log) {
-			console.log(message);
-		} else {
-			log(message);
-		}
+		if (normal_log) console.log(message);
+		else log(message);
 	}
 
-	if (stop === undefined) {
-		process.exit();
-	}
+	if (stop === undefined) process.exit();
 };
 // Use console.log over fancy-log.
 exit.normal = (messages, stop) => {

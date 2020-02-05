451e944136530f258d5d86d5caa4dff079923ec7
cgabriel5 <carlosgabriel71@yahoo.com>
2020-02-04 23:41:19 -0800

    chore: Add utils/cli.nim

---
+++ src/parser/nim/utils/cli.nim
@@ -0,0 +1,9 @@
+from os import getEnv, paramStr, paramCount
+
+# Like 'commandLineParams' but returns all CLI input in a string.
+#
+# @return {string} - The CLI input.
+proc input*(): string =
+    var output = ""
+    for i in 1..paramCount(): output &= paramStr(i)
+    return output

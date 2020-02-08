d2de6a3f5e5b0a3d9c385519cf36e990f2f240c8
cgabriel5 <carlosgabriel71@yahoo.com>
2020-02-07 19:14:24 -0800

    chore: Add parsers/newline.nim

---
+++ src/parser/nim/parser/parsers/newline.nim
@@ -0,0 +1,22 @@
+from tables import Table, `[]`, `$`, pairs
+from "../helpers/types" import State, node
+from "../helpers/tree_add" import add
+
+# ------------------------------------------------------------ Parsing Breakdown
+# \n
+# ^-Newline character.
+# ------------------------------------------------------------------------------
+
+# @param  {object} S - State object.
+# @return Nothing is returned.
+proc p_newline*(S: var State) =
+    var N = node(S, "NEWLINE")
+
+    N.start = S.i
+    N.end = S.i
+
+    S.line = S.line + 1
+    S.column = 0
+    S.sol_char = ""
+
+    add(S, N)

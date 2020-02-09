86bc8933af4b137aab8b8417b47d7cf2ca3e97ed
cgabriel5 <carlosgabriel71@yahoo.com>
2020-02-09 10:25:30 -0800

    update: Bring index.nim more in line with index.js

--- src/parser/nim/parser/index.nim
+++ src/parser/nim/parser/index.nim
@@ -1,20 +1,17 @@
 from tables import `[]=`, `[]`, hasKey, `$`
+from re import match
+# import tools/formatter
 from helpers/types import state
-
-# const error = require("./helpers/error.js");
-# const formatter = require("./tools/formatter.js");
-from parsers.newline import p_newline # import parsers / [newline]
-# const linetype = require("./helpers/line-type.js");
-# const specificity = require("./helpers/specificity.js");
-# const bracechecks = require("./helpers/brace-checks.js");
-# const { r_sol_char, r_space } = require("./helpers/patterns.js");
+import helpers/[brace_checks, error, linetype, specificity, tracer, rollback]
+from helpers/patterns import r_space, r_sol_char
+import parsers/newline
 
 proc parser*(action: string, text: string, cmdname: string, source: string,
     fmt: tuple, trace: bool, igc: bool, test: bool): int =
     var S = state(action, text, source, fmt, trace, igc, test)
     var linestarts = S.tables.linestarts
     # const stime = process.hrtime();
-    var line_type = ""
+    var ltype = ""
 
     var i = S.i
     let l = S.l
@@ -22,42 +19,40 @@ proc parser*(action: string, text: string, cmdname: string, source: string,
         S.i = S.i + 1
         S.column = S.column + 1
 
-        let char = text[S.i]
+        let `char` = text[S.i]
         var nchar: char
         if S.i + 1 < l: nchar = text[S.i + 1]
 
         # Handle newlines.
-        if char == '\n':
+        if `char` == '\n':
             p_newline(S)
             continue
 
         # Store line start index.
-        if not linestarts.hasKey(S.line): linestarts[S.line] = S.i
+        if not linestarts.hasKey(S.line): S.tables.linestarts[S.line] = S.i
+
+        # Start parsing at first non-ws character.
+        if S.sol_char == "" and not match($`char`, r_space):
+            S.sol_char = $`char`
+
+            # Sol char must be allowed.
+            if not match($`char`, r_sol_char): error(S, currentSourcePath, 10)
+
+            ltype = linetype(S, $`char`, $nchar)
+            if ltype == "terminator": break
+
+            specificity(S, ltype, currentSourcePath)
+
+            tracer.trace(S, ltype)
+            # require(`./parsers/${ltype}.js`)(S)
+
+    # Error if cc scope exists post-parsing.
+    bracechecks(S, check = "post-standing-scope")
 
     return 1
 
-    #     // Start parsing at first non-ws character.
-    #     if (!S.sol_char && !r_space.test(char)) {
-    #         S.sol_char = char;
-
-    #         // Sol char must be allowed.
-    #         if (!r_sol_char.test(char)) error(S, __filename, 10);
-
-    #         line_type = linetype(S, char, nchar);
-    #         if (line_type === "terminator") break;
-
-    #         specificity(S, line_type);
-
-    #         require("./helpers/trace.js")(S, line_type);
-    #         require(`./parsers/${line_type}.js`)(S);
-    #     }
-    # }
-
-    # // Error if cc scope exists post-parsing.
-    # bracechecks(S, null, "post-standing-scope");
-
-    # let res = {};
-    # if (action === "format") res.formatted = formatter(S);
-    # else res = require("./tools/acdef.js")(S, cmdname);
-    # res.time = process.hrtime(stime);
-    # return res;
+    # let res = {}
+    # if (action === "format") res.formatted = formatter(S)
+    # else res = require("./tools/acdef.js")(S, cmdname)
+    # res.time = process.hrtime(stime)
+    # return res

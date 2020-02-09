598cea73eda4daf5cdb30f843ea9fb9238ff8dfe
cgabriel5 <carlosgabriel71@yahoo.com>
2020-02-09 10:22:15 -0800

    chore: Add helpers/linetype.nim

---
+++ src/parser/nim/parser/helpers/linetype.nim
@@ -0,0 +1,35 @@
+from tables import toTable, hasKey, `[]`, `$`
+from re import match
+from patterns import r_space, r_letter
+from "../helpers/types" import State
+
+# Determine line's line type.
+#
+# @param  {object} S - State object.
+# @param  {char} char - The loop's current character.
+# @param  {char} nchar - The loop's next character.
+# @return {string} - The line's type.
+proc linetype*(S: State, char: string, nchar: string): string =
+    let text = S.text
+
+    const types = {
+        ";": "terminator", # End parsing.
+        "#": "comment",
+        "$": "variable",
+        "@": "setting",
+        "-": "flag",
+        ")": "close-brace",
+        "]": "close-brace"
+    }.toTable
+
+    let echar = ""
+    var line_type = if types.hasKey(char): types[char] else: echar
+
+    # Line type overrides for: command, option, default.
+    if line_type == "" and match(char, r_letter): line_type = "command"
+    if line_type == "flag":
+        if nchar != "" and match(nchar, r_space): line_type = "option"
+    elif line_type == "command":
+        if text[S.i .. ^7] == "default": line_type = "flag"
+
+    return line_type

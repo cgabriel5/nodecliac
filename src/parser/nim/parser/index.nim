ff1909618213279375da4e2111aab0cef9759bb7
cgabriel5 <carlosgabriel71@yahoo.com>
2020-02-11 15:59:16 -0800

    update: Use while loop over for loop in nim/parser/index.nim

--- src/parser/nim/parser/index.nim
+++ src/parser/nim/parser/index.nim
@@ -2,7 +2,7 @@ from tables import `[]=`, `[]`, hasKey, `$`
 from re import match
 # import tools/formatter
 from helpers/types import state
-import helpers/[brace_checks, error, linetype, specificity, tracer, rollback]
+import helpers/[brace_checks, error, linetype, specificity, tracer, rollback, forward]
 from helpers/patterns import r_space, r_sol_char
 import parsers/newline
 
@@ -13,19 +13,16 @@ proc parser*(action: string, text: string, cmdname: string, source: string,
     # const stime = process.hrtime();
     var ltype = ""
 
-    var i = S.i
-    let l = S.l
-    for i in countup(1, l - 1, 1):
-        S.i = S.i + 1
-        S.column = S.column + 1
-
-        let `char` = text[S.i]
-        var nchar: char
-        if S.i + 1 < l: nchar = text[S.i + 1]
+    let i = S.i; let l = S.l; var `char`, nchar: char
+    while S.i < S.l:
+        `char` = text[S.i]
+        nchar = if S.i + 1 < l: text[S.i + 1] else: '\0'
+        # [https://stackoverflow.com/a/18410258]
 
         # Handle newlines.
         if `char` == '\n':
             p_newline(S)
+            forward(S)
             continue
 
         # Store line start index.
@@ -45,6 +42,7 @@ proc parser*(action: string, text: string, cmdname: string, source: string,
 
             tracer.trace(S, ltype)
             # require(`./parsers/${ltype}.js`)(S)
+        forward(S)
 
     # Error if cc scope exists post-parsing.
     bracechecks(S, check = "post-standing-scope")

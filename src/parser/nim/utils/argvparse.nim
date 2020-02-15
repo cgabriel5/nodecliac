c5248ce8d6162b03a369e8c9ff49aba2c8c56e51
cgabriel5 <carlosgabriel71@yahoo.com>
2020-02-15 11:05:41 -0800

    refactor: Cleanup util/argvparse

--- src/parser/nim/utils/argvparse.nim
+++ src/parser/nim/utils/argvparse.nim
@@ -7,41 +7,37 @@ from parseopt import next, cmdEnd, cmdArgument,
 from fs import expand_tilde
 
 if os.paramCount() == 0: quit() # Exit if no args.
-let args = commandLineParams()
 
-type Arguments = ref object of RootObj
-    action*: string
-    source*: string
-    indent*: string
-    print*: bool
-    trace*: bool
-    test*: bool
-    igc*: bool
-let Args = Arguments(action: args[0])
+type Arguments = ref object
+    action*, source*, indent*: string
+    print*, trace*, test*, igc*: bool
 
 # Add value to key in Args object.
 #
 # @return - Nothing is returned.
-proc set(s: string, nitem: string, boolval: string) =
-    if s == "source": Args.source = nitem
-    elif s == "indent": Args.indent = nitem
+proc set(A: Arguments, s: string, nitem: string, boolval: string) =
+    if s == "source": A.source = nitem
+    elif s == "indent": A.indent = nitem
     else:
         let val = if boolval == "1": true else: false
         case (s):
-        of "print": Args.print = val
-        of "trace": Args.trace = val
-        of "test": Args.test = val
-        of "strip-comments": Args.igc = val
+        of "print": A.print = val
+        of "trace": A.trace = val
+        of "test": A.test = val
+        of "strip-comments": A.igc = val
         else: discard
 
 # Get needed arguments from CLI input.
 #
 # @return {object} - Object containing needed CLI arguments.
 proc argvparse*(): Arguments =
+    new(result)
+
+    let args = commandLineParams()
     var p = initOptParser(
-        args[1..args.len - 1],
+        args[1 .. args.high], # Exclude action.
         allowWhitespaceAfterColon = false
-    ) # Exclude action.
+    )
     var i = 1
     let l = args.len;
     let r = re"^-*"
@@ -54,14 +50,14 @@ proc argvparse*(): Arguments =
             if p.val == "" and flag != p.key & "=": # Handle --key value (no '=')
                 if i + 1 < l and not args[i + 1].startsWith('-'): # Lookahead
                     let nitem = args[i + 1]
-                    set(p.key, nitem, if nitem == "false": "0" else: "1")
+                    set(result, p.key, nitem, if nitem == "false": "0" else: "1")
                     inc(i)
                     p.next()
-                else: set(p.key, "", "1")
+                else: set(result, p.key, "", "1")
             else: # Handle --key=value
-                set(p.key, p.val, if p.val == "false": "0" else: "1")
+                set(result, p.key, p.val, if p.val == "false": "0" else: "1")
         of cmdArgument: discard
         inc(i)
 
-    Args.source = expand_tilde(Args.source)
-    return Args
+    result.action = args[0]
+    result.source = expand_tilde(result.source)

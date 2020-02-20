26354dc5f889c99dcc69ccabb259e998340fc046
cgabriel5 <carlosgabriel71@yahoo.com>
2020-02-20 14:53:36 -0800

    chore: Add nim/utils/regexp.nim

---
+++ src/parser/nim/utils/regex.nim
@@ -0,0 +1,18 @@
+from re import Regex, findBounds
+
+# Like findBounds() but returns all found bounds in a sequence.
+#
+# @param  {string} s - The string to search.
+# @param  {regex} pattern - The RegExp to search against.
+# @return {sequence} - Sequence containing all bounds.
+proc findAllBounds*(s: string, pattern: Regex): seq[tuple[first, last: int]] =
+    var bounds: seq[tuple[first, last: int]] = @[]
+    var offset = 0
+
+    while true:
+        var res = findBounds(s, pattern, start=offset)
+        if res.first == -1: break
+        bounds.add(res)
+        offset = res.last
+
+    result = bounds

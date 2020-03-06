8af590713512f09e9592b9185c6988c70e058d2d
cgabriel5 <carlosgabriel71@yahoo.com>
2020-03-06 10:50:50 -0800

    fix: Revert pre-parse.pl

--- resources/packages/yarn/hooks/pre-parse.pl
+++ resources/packages/yarn/hooks/pre-parse.pl
@@ -7,13 +7,12 @@
 # use diagnostics;
 
 my $input = $ARGV[0];
-my $output = "";
-
+my $output = "\n";
 my ($workspace_name, $cli_remainder) = $input =~ /^[ \t]*?yarn[ \t]+?workspace[ \t]+?([^ \t]+?)[ \t]+?(.*)/;
 
 # ----- Input logic -----
 
-if ($cli_remainder) { $output .= ">cline\nyarn $cli_remainder\n<"; }
+if ($cli_remainder) { $output = "yarn $cli_remainder"; }
 
 # ----- ACDEF logic -----
 
@@ -23,7 +22,6 @@ if ($input =~ /^[ \t]*?yarn[ \t]+?([^ \t]*?)$/) {
 
 	# If a workspace use its location.
 	if ($workspace_name) { $cwd .= "/$1"; }
-
 	while ($cwd) {
 		if (-e "$cwd/package.json") { $pkg = "$cwd/package.json"; last; }
 		$cwd = substr($cwd, 0, rindex($cwd, '/'));
@@ -32,10 +30,9 @@ if ($input =~ /^[ \t]*?yarn[ \t]+?([^ \t]*?)$/) {
 	if ($pkg) {
 		my $pkgcontents = do{local(@ARGV,$/)=$pkg;<>};
 		if ($pkgcontents =~ /"scripts"\s*:\s*{([\s\S]*?)}(,|$)/) {
-			$output .= ">acdef\n";
 			my @matches = ($1 =~ /"([^"]*)"\s*:/g);
 			foreach (@matches) { $output .=  '.' . $_ . " --\n"; }
-			$output .= '<';
+			chomp($output);
 		}
 	}
 }

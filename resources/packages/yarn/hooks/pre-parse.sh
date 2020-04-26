41a2d74baf125c843c921945cb8f2458f8c182cc
cgabriel5 <carlosgabriel71@yahoo.com>
2020-04-26 01:09:17 -0700

    update: Cleanup resources/packages/yarn/hooks/pre-parse.sh

--- resources/packages/yarn/hooks/pre-parse.sh
+++ resources/packages/yarn/hooks/pre-parse.sh
@@ -1,30 +1,18 @@
 #!/bin/bash
 
-# Change the following variables.
-# $COMP_LINE    # Original (complete) CLI input.
-# $cpoint       # Caret index when [TAB] key was pressed.
-# $maincommand  # The command name making completions for.
-# $acdef        # The ACDEF definitions file contents.
+# Initialization variables:
+#
+# cline    # CLI input.
+# cpoint   # Index of caret position when [TAB] key was pressed.
+# command  # Program for which completions are for.
+# acdef    # The command's .acdef file contents.
 
-# This script will run the Perl pre-parse script. The returned data is in the
-# following format: the 1st line is the modified CLI input while subsequent
-# lines are addons to the ACDEF.
-output="$("$HOME/.nodecliac/registry/yarn/hooks/pre-parse.pl" "$COMP_LINE")"
+output="$("$HOME/.nodecliac/registry/$command/hooks/pre-parse.pl" "$cline")"
 
-# Get modified CLI input line (1st line).
-# [https://stackoverflow.com/q/30649640]
-# firstline=$(LC_ALL=C perl -npe "exit if $. > 1" <<< "$output")
+# 1st line is the modified CLI (workspace) input.
 read -r firstline <<< "$output"
-if [[ -n "$firstline" ]]; then cline="$firstline"; fi
+[[ -n "$firstline" ]] && cline="$firstline"
 
-# Get ACDEF addon entries.
-# addon=$(LC_ALL=C perl -ne "print if $. > 1" <<< "$output")
-# [https://stackoverflow.com/a/24542788]
-# addon=$(awk 'NR>1' <<< "$output")
-
-# Remove first line from output. Remaining text, if any, is the ACDEF output.
-len="${#firstline}"
-# If length is zero, reset length to 1 to remove starting new line.
-if [[ $len == 0 ]]; then len=1; fi
-addon="${output:$len}"
-if [[ -n "$addon" ]]; then acdef+=$'\n'"$addon"; fi
+# Remaining lines are package.json's script entries.
+len="${#firstline}"; [[ ! "$len" ]] || len=1
+addon="${output:$len}"; [[ -n "$addon" ]] && acdef+=$'\n'"$addon"

481760302e6f6154a3cc68bfc830f6bc6c1a7b8a
cgabriel5 <carlosgabriel71@yahoo.com>
2020-07-26 12:42:52 -0700

    chore: Add test to nodecliac.acmap

--- resources/packages/nodecliac/nodecliac.tests.sh
+++ resources/packages/nodecliac/nodecliac.tests.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 
-# DON'T EDIT FILE —— GENERATED: Sat Jul 18 2020 20:19:34 (1595128774)
+# DON'T EDIT FILE —— GENERATED: Sun Jul 26 2020 12:42:11 (1595792531)
 
 tests=(
-"nodecliac ; *uninstall*"
-"nodecliac --engine=2 --; *--version *"
-"nodecliac format; *format*"
-"nodecliac format positional_arg; !*format*"
-"nodecliac format positional_arg ; !*format*"
-"nodecliac print --command=; *subl*"
-"nodecliac print --command; *--command=*"
-"nodecliac print --command=node; *nodecliac *"
-"nodecliac print --command node; *nodecliac *"
-"nodecliac print --command ; *nodecliac *"
-"nodecliac print --comm; *--command*"
-"nodecliac make --sou| path/to/file; *source*"
-"nodecliac make --source install --print --so; !*--source*"
-'nodecliac format --source command.acmap --print --indent "s:2" --; *strip-comments*'
+"nodecliac ;*uninstall*"
+"nodecliac --engine=2 --;*--version *"
+"nodecliac format;*format*"
+"nodecliac format positional_arg;!*format*"
+"nodecliac format positional_arg ;!*format*"
+"nodecliac print --command=;*subl*"
+"nodecliac print --command;*--command=*"
+"nodecliac print --command=node;*nodecliac *"
+"nodecliac print --command node;*nodecliac *"
+"nodecliac print --command ;*nodecliac *"
+"nodecliac print --comm;*--command*"
+"nodecliac make --sou| path/to/file;*source*"
+"nodecliac make --source install --print --so;!*--source*"
+'nodecliac format --source command.acmap --print --indent "s:2" --;*strip-comments*'
+'nodecliac NONEXISTANTCOMMAND ;!*format*'
 )

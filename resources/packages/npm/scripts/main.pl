b2e1af7c2012e85327da8821989d9bc73798af1a
cgabriel5 <carlosgabriel71@yahoo.com>
2020-08-20 09:04:59 -0700

    update: Modify npm/scripts/main.pl for npm use

--- resources/packages/npm/scripts/main.pl
+++ resources/packages/npm/scripts/main.pl
@@ -5,71 +5,84 @@
 # use diagnostics;
 
 my $action = $ARGV[0];
-my $useglobal = $ARGV[1];
+my $useglobal = 0;
 my $cwd = $ENV{'PWD'};
 my $hdir = $ENV{'HOME'};
-my $input = $ENV{'NODECLIAC_INPUT_ORIGINAL'};
+my $usedflags = $ENV{'NODECLIAC_USED_FLAGS'};
 my $pkg = '';
 
-if (!$useglobal) {
-	# If a workspace use its location.
-	if ($input =~ /^[ \t]*?yarn[ \t]+?workspace[ \t]+?([^ \t]+?)[ \t]+?.*/) { $cwd = "/$1"; }
-
-	while ($cwd) {
-		if (-e "$cwd/package.json") { $pkg = "$cwd/package.json"; last; }
-		$cwd = substr($cwd, 0, rindex($cwd, '/'));
-	}
-} else {
-	my @paths = (
-		"$hdir/.config/yarn/global/package.json",
-		"$hdir/.local/share/yarn/global/package.json",
-		"$hdir/.yarn/global/package.json"
-	);
-
-	foreach my $path (@paths) { if (-f $path) { $pkg = $path; last; } }
-}
-
-my %args;
-
-if ($action eq 'run') {
-	if ($pkg) {
-		my $pkgcontents = do{local(@ARGV,$/)=$pkg;<>};
-		if ($pkgcontents =~ /"scripts"\s*:\s*{([\s\S]*?)}(,|$)/) {
-			my @matches = ($1 =~ /"([^"]*)"\s*:/g);
-			foreach (@matches) { $args{"$_"} = undef; }
-		}
-	}
-} elsif ($action eq 'workspace') {
-	my $workspaces_info = `LC_ALL=C yarn workspaces info -s 2> /dev/null`;
-	my $args_count = $ENV{'NODECLIAC_ARG_COUNT'};
-
-	if (($workspaces_info && $args_count <= 2) || ($workspaces_info && $args_count <= 3 && $ENV{'NODECLIAC_LAST_CHAR'})) {
-		# Get workspace names.
-		while ($workspaces_info =~ /"location":\s*"([^"]+)",/g) { $args{"$1"} = undef; }
-	}
-} else { # Remaining actions: remove|outdated|unplug|upgrade
-	if ($pkg) {
-		my $pkgcontents = do{local(@ARGV,$/)=$pkg;<>};
-		my @matches = ($pkgcontents =~ /"(?:dependencies|devDependencies)"\s*:\s*{([\s\S]*?)}(,|$)/g);
-		foreach my $match (@matches) {
-			my @deps = ($match =~ /"([^"]*)"\s*:/g);
-			foreach (@deps) { $args{"$_"} = undef; }
-		}
-	}
-}
-
-# my $last = $ENV{'NODECLIAC_LAST'};
-# my $lchar = $ENV{'NODECLIAC_LAST_CHAR'};
-# my $nchar = $ENV{'NODECLIAC_NEXT_CHAR'};
 my $used = $ENV{'NODECLIAC_USED_DEFAULT_POSITIONAL_ARGS'};
 chomp($used); # Remove trailing newline.
 my @used_args = split(/[\n ]/, $used);
 
-foreach my $uarg (@used_args) {
-	if (exists($args{$uarg})) {
-		delete $args{$uarg};
-		$args{"!$uarg"} = undef;
+my $delimiter = "\$\\r\?\\n";
+my @flags = split(/$delimiter/m, $usedflags);
+for my $flag (@flags) {
+	if ($flag eq "-g" || $flag eq "--global") {
+		$useglobal = 1;
+		last;
 	}
 }
 
-print join("\n", (keys %args));
+if (!$useglobal) {
+	while ($cwd) {
+		if (-e "$cwd/package.json") { $pkg = "$cwd/package.json"; last; }
+		$cwd = substr($cwd, 0, rindex($cwd, '/'));
+	}
+
+	my %args;
+
+	if ($action eq 'run') {
+		if ($pkg) {
+			my $pkgcontents = do{local(@ARGV,$/)=$pkg;<>};
+			if ($pkgcontents =~ /"scripts"\s*:\s*{([\s\S]*?)}(,|$)/) {
+				my @matches = ($1 =~ /"([^"]*)"\s*:/g);
+				foreach (@matches) { $args{"$_"} = undef; }
+			}
+		}
+	} else { # Remaining actions: remove|outdated|unplug|upgrade
+		if ($pkg) {
+			my $pkgcontents = do{local(@ARGV,$/)=$pkg;<>};
+			my @matches = ($pkgcontents =~ /"(?:dependencies|devDependencies)"\s*:\s*{([\s\S]*?)}(,|$)/g);
+			foreach my $match (@matches) {
+				my @deps = ($match =~ /"([^"]*)"\s*:/g);
+				foreach (@deps) { $args{"$_"} = undef; }
+			}
+		}
+	}
+
+	foreach my $uarg (@used_args) {
+		if (exists($args{$uarg})) {
+			delete $args{$uarg};
+			$args{"!$uarg"} = undef;
+		}
+	}
+
+	print join("\n", (keys %args));
+
+} else {
+	my $rootdir = "$hdir/.nodecliac/registry/npm/.rootdir";
+	if (! -e $rootdir) {
+		open(FH, '>', $rootdir) or die;
+		# [https://stackoverflow.com/a/5926706]
+		# [https://flaviocopes.com/where-npm-install-packages/]
+		print FH `npm root -g`;
+		close(FH);
+	}
+
+	my $root = do{local(@ARGV,$/)=$rootdir;<>};
+	$root=~ s/\s+$//;
+
+	# [https://stackoverflow.com/a/5751949]
+	# [https://stackoverflow.com/a/2912084]
+	# [https://alvinalexander.com/blog/post/perl/how-process-every-file-directory-perl/]
+	# [https://stackoverflow.com/a/37786730]
+	opendir(DIR, $root) or exit;
+	while (my $name = readdir(DIR)) {
+		if (-d "$root/$name" && $name !~ /^\.{1,2}$/) {
+			my $pattern = '^' . $name . '$';
+			print (($used =~ /$pattern/m ? '!' : '') . "$name\n");
+		}
+	}
+	closedir(DIR);
+}

4b4f9f25dad5acf978ed2d08d73c4e04f516d256
cgabriel5 <carlosgabriel71@yahoo.com>
2020-08-24 00:19:19 -0700

    update: Add 1password pre-parse hook to handle 'op help ' command

---
+++ resources/packages/op/hooks/pre-parse.pl
@@ -0,0 +1,10 @@
+#!/usr/bin/perl
+
+# When runninh returns repo scripts to add to ACDEF and the modified CLI input.
+
+# use strict;
+# use warnings;
+# use diagnostics;
+
+my $input = $ARGV[0];
+if ($input =~ /^[ \t]*?op[ \t]+?help[ \t](.*)/) { print "op $1"; }

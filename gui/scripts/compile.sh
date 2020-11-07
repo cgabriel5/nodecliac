d4011a23721d50e02490e16f855c1920e09d4eba
cgabriel5 <cgabriel5@users.noreply.github.com>
2020-11-07 15:36:13 -0800

    chore: Update GUI compile script options

--- gui/scripts/compile.sh
+++ gui/scripts/compile.sh
@@ -3,14 +3,22 @@
 # [Bug:sink] Don't use `--gc:arc` -> [https://github.com/nim-lang/Nim/issues/15238]
 # [https://github.com/juancarlospaco/webgui/blob/master/src/webgui.nim.cfg]
 
+# --app:gui [https://stackoverflow.com/a/36346102]
+
 nim compile \
+--app:gui \
+-d:nimDebugDlOpen \
 --forceBuild:on \
+-d:ssl \
 -d:release \
 -d:danger \
 --tlsEmulation:off \
---exceptions:quirky \
+--debugger:off \
+--debuginfo:off \
 --panics:on \
 --passL:"-s" \
+--threads:on \
+--verbosity:0 \
 --passC:"-flto -ffast-math -march=native -mtune=native -fsingle-precision-constant" \
 -d:noSignalHandler \
 main.nim

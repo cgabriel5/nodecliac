8ad0d6d5d243b890774d5e68a36b01a5db6ecdba
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-03-01 14:08:21 -0800

    update: Add nim/utils/osutils.nim

---
+++ src/parser/nim/utils/osutils.nim
@@ -0,0 +1,8 @@
+from distros import Distribution, detectOs
+
+proc platform*(): string =
+    if detectOs(Linux): return "linux"
+    if detectOs(MacOSX): return "macosx"
+    if detectOs(Windows): return "windows"
+    if detectOs(BSD): return "bsd"
+    return "unknown"

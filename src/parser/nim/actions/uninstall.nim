33a5d6ede28712909445c5df1f65050268c9bdf2
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-03-02 13:28:52 -0800

    update: Add ported actions/uninstall.nim

---
+++ src/parser/nim/actions/uninstall.nim
@@ -0,0 +1,38 @@
+import json
+
+proc uninstall() {.async.} =
+    let ncliacdir = paths["ncliacdir"]
+    var bashrcpath = paths["bashrcpath"]
+    let setupfilepath = paths["setupfilepath"]
+
+    # [https://stackoverflow.com/a/84899]
+    discard execProcess("sudo sh -c '' > /dev/null 2>&1") # Prompt password.
+
+    # Delete nodecliac dir.
+    if dirExists(ncliacdir): removeDir(ncliacdir)
+
+    # Get bashrc file contents.
+    if rcfile.len != 0: bashrcpath = rcfile
+    else:
+        if fileExists(setupfilepath):
+            let res = readFile(setupfilepath)
+            if res.len != 0:
+                bashrcpath = parseJSON(res){"rcfile"}.getStr(bashrcpath)
+
+    # Remove .rcfile modifications.
+    if bashrcpath.len != 0:
+        if fileExists(bashrcpath):
+            var res = readFile(bashrcpath)
+            if res.contains(re("^ncliac=~", {reMultiLine})):
+                res = res.replace(re("""([# \t]*)\bncliac.*"\$ncliac";?\n?"""), "")
+                writeFile(bashrcpath, res)
+
+                let varg1 = "success".chalk("green")
+                let varg2 = bashrcpath.chalk("bold")
+                echo fmt"{varg1} reverted {varg2} changes."
+
+    # Remove bin file.
+    let binfilepath = "/usr/local/bin/nodecliac"
+    if fileExists(binfilepath) and "#!/bin/bash" in readFile(binfilepath).strip():
+        discard execProcess(fmt"sudo rm -rf {binfilepath} > /dev/null 2>&1")
+        echo "success".chalk("green") & " removed nodecliac bin file."

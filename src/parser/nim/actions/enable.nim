b73f3f9a5e3f1b1420ecf1c4a23889d1b56a355a
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-03-01 20:02:25 -0800

    update: Add ported actions/enable.nim

---
+++ src/parser/nim/actions/enable.nim
@@ -0,0 +1,37 @@
+import sequtils, algorithm, os, strutils
+
+proc enable() {.async.} =
+    var packages = paramsargs
+    let action = packages[0]
+    packages.delete(0, 0)
+
+    let state = if action == "enable": "false" else: "true"
+
+    # Get all packages when '--all' is provided.
+    if all:
+        packages.setLen(0)
+        packages = (
+            const dirtypes = {pcDir, pcLinkToDir}
+            for kind, path in walkDir(registrypath):
+                if kind notin dirtypes: continue
+                let parts = splitPath(path)
+                packages.add(parts.tail)
+            packages.sort()
+            packages
+        )
+
+    # Loop over packages and remove each if its exists.
+    for pkg in packages:
+        let filepath = fmt"{registrypath}/{pkg}/.{pkg}.config.acdef"
+        let resolved_path = absolutePath(filepath)
+        # Workout symlinks: expandSymlink(), symlinkExists()
+
+        if not fileExists(resolved_path): continue
+        var contents = readFile(resolved_path).strip()
+
+        contents = contents.replace(re("^@disable[^\n]*", {reMultiLine}), "").strip()
+        contents &= fmt"\n@disable = {state}\n"
+        contents = contents.replace(re("^\n/", {reMultiLine}), "") # Remove newlines.
+        contents = contents.replace(re("\n", {reMultiLine}), "\n\n") # Add newline after header.
+
+        writeFile(filepath, contents)

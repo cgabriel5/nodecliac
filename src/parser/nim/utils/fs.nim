88ccc70f923f5b1c212f40f14bd4729e24c7b04d
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-03-03 11:22:06 -0800

    update: Add file permission helper functions

--- src/parser/nim/utils/fs.nim
+++ src/parser/nim/utils/fs.nim
@@ -1,9 +1,49 @@
 from strutils import split
-from os import paramStr, paramCount, splitFile, DirSep
+from os import paramStr, paramCount, splitFile, DirSep,
+    setFilePermissions, FilePermission
 from streams import close, readAll, write, newFileStream, openFileStream
 
 type FileInfo = ref object
     name*, dirname*, ext*, path*: string
+
+# Convenience func to convert Unix like file permission to ``set[FilePermission]``.
+#
+# @param  {number} perm - The permissions in octal form.
+# @resource [https://github.com/nim-lang/fusion/blob/master/src/fusion/filepermissions.nim]
+# @resource [https://forum.nim-lang.org/t/7476]
+proc toFilePermissions*(perm: Natural): set[FilePermission] =
+    var perm = uint(perm)
+    for permBase in [fpOthersExec, fpGroupExec, fpUserExec]:
+        if (perm and 1) != 0: result.incl permBase         # Exec
+        if (perm and 2) != 0: result.incl permBase.succ()  # Read
+        if (perm and 4) != 0: result.incl permBase.succ(2) # Write
+        perm = perm shr 3  # Shift to next permission group
+
+# Convenience func to convert ``set[FilePermission]`` to Unix like file permission.
+#
+# @param  {set} perm - The permissions in a set[FilePermission].
+# @resource [https://github.com/nim-lang/fusion/blob/master/src/fusion/filepermissions.nim]
+# @resource [https://forum.nim-lang.org/t/7476]
+proc fromFilePermissions*(perm: set[FilePermission]): uint =
+    if fpUserExec in perm:    inc result, 0o100  # User
+    if fpUserWrite in perm:   inc result, 0o200
+    if fpUserRead in perm:    inc result, 0o400
+    if fpGroupExec in perm:   inc result, 0o010  # Group
+    if fpGroupWrite in perm:  inc result, 0o020
+    if fpGroupRead in perm:   inc result, 0o040
+    if fpOthersExec in perm:  inc result, 0o001  # Others
+    if fpOthersWrite in perm: inc result, 0o002
+    if fpOthersRead in perm:  inc result, 0o004
+
+# Convenience proc for `os.setFilePermissions("file.ext", filepermissions.toFilePermissions(0o666))`
+# to change file permissions using Unix like octal file permission.
+#
+# @param  {string} path - The file path.
+# @param  {number} permissions - The permissions in octal form.
+# @resource [https://github.com/nim-lang/fusion/blob/master/src/fusion/filepermissions.nim]
+# @resource [https://forum.nim-lang.org/t/7476]
+proc chmod*(path: string; permissions: Natural) {.inline.} =
+    setFilePermissions(path, toFilePermissions(permissions))
 
 # Get file path information (i.e. file name and directory path).
 #

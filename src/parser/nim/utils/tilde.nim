20ca56408fc8a2cba2d24eee5aba5ad5e73671de
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-03-09 11:02:28 -0800

    fix: Correct runnable tests

--- src/parser/nim/utils/tilde.nim
+++ src/parser/nim/utils/tilde.nim
@@ -1,18 +1,21 @@
 import os, strutils
 
-let hdir = os.getEnv("HOME")
+let hdir = getEnv("HOME")
 
 # Expands starting tilde ('~') in path.
 #
 # @param {string} 1) - Path to expand.
 # @return {string} - The expanded path.
 #
-# @resource: [https://nim-lang.org/docs/os.html#expandTilde%2Cstring]
+# @resource: [https://nim-lang.org/docs/html#expandTilde%2Cstring]
 proc expand*(p: string): string =
     runnableExamples:
-      doAssert expand("") == ""
-      doAssert expand("~") == hdir
-      doAssert expand("~/Desktop") == joinPath(hdir, "Desktop")
+        import os
+        let hdir = getEnv("HOME")
+
+        doAssert expand("") == ""
+        doAssert expand("~") == hdir
+        doAssert expand("~/Desktop") == joinPath(hdir, "Desktop")
 
     result = p
     if p.len == 0: result = ""
@@ -24,9 +27,12 @@ proc expand*(p: string): string =
 # @return {undefined} - Nothing is returned.
 proc shrink*(p: string): string =
     runnableExamples:
-      doAssert shrink("") == ""
-      doAssert shrink(hdir) == "~"
-      doAssert shrink(hdir & "/Desktop") == "~/Desktop"
+        import os
+        let hdir = getEnv("HOME")
+
+        doAssert shrink("") == ""
+        doAssert shrink(hdir) == "~"
+        doAssert shrink(hdir & "/Desktop") == "~/Desktop"
 
     result = p
     if p.startsWith(hdir): result = "~" & p[hdir.len .. ^ 1]

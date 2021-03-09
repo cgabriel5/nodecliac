f16db6c726dea9cf886154e9d158d2fdea5d0353
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-03-09 10:56:36 -0800

    update: Add vla.nim to nim/utils/
    - File adds variable length arrays to Nim.

---
+++ src/parser/nim/utils/vla.nim
@@ -0,0 +1,40 @@
+# [https://github.com/bpr/vla]
+# [https://forum.nim-lang.org/t/4904#30701]
+# [https://forum.nim-lang.org/t/499]
+
+include system/ansi_c
+
+proc alloca(n: int): pointer {.importc, header: "<alloca.h>".}
+
+type
+    VarLengthArray*[T] =
+        ptr object
+            len: int
+            data: UncheckedArray[T]
+
+proc `[]`*[T](a: VarLengthArray[T], i: int): T {.inline.} =
+    assert i >= 0 and i < a.len
+    result = a.data[i]
+
+proc `[]=`*[T](a: VarLengthArray[T], i: int, x: T) {.inline.} =
+    assert i >= 0 and i < a.len
+    a.data[i] = x
+
+proc len*[T](a: VarLengthArray[T]): int =
+    a.len
+
+template newVLA*(T: typedesc, n: int): untyped =
+    let bytes = sizeof(int) + sizeof(T)*n
+    var vla = cast[VarLengthArray[T]](alloca(bytes))
+    c_memset(vla, cast[cint](0), cast[csize_t](bytes))
+    vla.len = n
+    vla
+
+# Untested code
+template asOpenArray*[T](a: VarLengthArray[T]): openarray[T] =
+    cast[seq[type(a[0])]](a)
+
+proc toSeq*[T](a: VarLengthArray[T]): seq[T] =
+    result = newSeq[T](len(a))
+    for i in 0..len(a)-1:
+        result[i] = a[i]

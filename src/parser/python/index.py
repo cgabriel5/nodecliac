bfac0e3ff852c2f50a7147b7c5c959be8d715cb8
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-05-23 01:56:56 -0700

    update: Wrap code in main function

--- src/parser/python/index.py
+++ src/parser/python/index.py
@@ -9,131 +9,136 @@ from parser import parser
 from utils.chalk import chalk
 from utils.fs import info, write
 
-if len(sys.argv) == 1: quit()
+def main():
+    if __name__ == "__main__":
 
-# [https://stackoverflow.com/a/30493366]
-# [https://blender.stackexchange.com/a/8405]
-# [https://stackoverflow.com/a/12818237]
-cliparser = argparse.ArgumentParser()
-cliparser.add_argument("--igc", default=False)
-cliparser.add_argument("--test", default=False)
-cliparser.add_argument("--print", default=False)
-cliparser.add_argument("--trace", default="")
-cliparser.add_argument("action", default="")
-# [https://stackoverflow.com/a/15301183]
-cliparser.add_argument("--indent", default="s:4")
-cliparser.add_argument("--source", default="")
-args, unknown = cliparser.parse_known_args()
+        if len(sys.argv) == 1: quit()
 
-igc = args.igc
-test = args.test
-print_ = args.print
-trace = args.trace
-action = args.action
-indent = args.indent
-source = args.source
-formatting = action == "format"
+        # [https://stackoverflow.com/a/30493366]
+        # [https://blender.stackexchange.com/a/8405]
+        # [https://stackoverflow.com/a/12818237]
+        cliparser = argparse.ArgumentParser()
+        cliparser.add_argument("--igc", default=False)
+        cliparser.add_argument("--test", default=False)
+        cliparser.add_argument("--print", default=False)
+        cliparser.add_argument("--trace", default="")
+        cliparser.add_argument("action", default="")
+        # [https://stackoverflow.com/a/15301183]
+        cliparser.add_argument("--indent", default="s:4")
+        cliparser.add_argument("--source", default="")
+        args, unknown = cliparser.parse_known_args()
 
-fmtinfo = ['\t', 1] # (char, amount)
-# Parse/validate indentation.
-if formatting and indent != "":
-    r = r"^(s|t):\d+$"
-    re.search(r, indent)
-    # [https://stackoverflow.com/a/38342048]
-    if re.search(r, indent) == None:
-        print("Invalid indentation string.")
-        quit()
-    components = indent.split(":", 2)
-    fmtinfo[0] = ' ' if components[0] == 's' else '\t'
-    fmtinfo[1] = int(components[1])
+        igc = args.igc
+        test = args.test
+        print_ = args.print
+        trace = args.trace
+        action = args.action
+        indent = args.indent
+        source = args.source
+        formatting = action == "format"
 
-# Source must be provided.
-if not source:
-    print("Please provide a " + chalk("--source", "bold") + " path.")
-    exit()
+        fmtinfo = ['\t', 1] # (char, amount)
+        # Parse/validate indentation.
+        if formatting and indent != "":
+            r = r"^(s|t):\d+$"
+            re.search(r, indent)
+            # [https://stackoverflow.com/a/38342048]
+            if re.search(r, indent) == None:
+                print("Invalid indentation string.")
+                quit()
+            components = indent.split(":", 2)
+            fmtinfo[0] = ' ' if components[0] == 's' else '\t'
+            fmtinfo[1] = int(components[1])
 
-# Breakdown path.
-fi = info(source)
-extension = fi["ext"]
-cmdname = re.sub(rf"\.{extension}$", "", fi["name"]) # [TODO] `replace`
-dirname = fi["dirname"]
+        # Source must be provided.
+        if not source:
+            print("Please provide a " + chalk("--source", "bold") + " path.")
+            exit()
 
-# Make path absolute.
-if not os.path.isabs(source): source = os.path.abspath(source)
+        # Breakdown path.
+        fi = info(source)
+        extension = fi["ext"]
+        cmdname = re.sub(rf"\.{extension}$", "", fi["name"]) # [TODO] `replace`
+        dirname = fi["dirname"]
 
-if os.path.isdir(source):
-    print("Directory provided but .acmap file path needed.")
-    quit()
-if not os.path.isfile(source):
-    print("Path " + chalk(source, "bold") + " doesn't exist.")
-    quit()
+        # Make path absolute.
+        if not os.path.isabs(source): source = os.path.abspath(source)
 
-# [TODO] Look into why using `let` over `var` causes hang up when file
-# is large. Possibly due to `openFileStream`?
-f = open(source, "r")
-res = f.read()
+        if os.path.isdir(source):
+            print("Directory provided but .acmap file path needed.")
+            quit()
+        if not os.path.isfile(source):
+            print("Path " + chalk(source, "bold") + " doesn't exist.")
+            quit()
 
-(
-    acdef, config, keywords, filedirs,
-    contexts, formatted, placeholders, tests
-) = \
-parser(
-    action, res, cmdname,
-    source, fmtinfo, trace, igc, test
-)
+        # [TODO] Look into why using `let` over `var` causes hang up when file
+        # is large. Possibly due to `openFileStream`?
+        f = open(source, "r")
+        res = f.read()
 
-testname = cmdname + ".tests.sh"
-savename = cmdname + ".acdef"
-saveconfigname = "." + cmdname + ".config.acdef"
+        (
+            acdef, config, keywords, filedirs,
+            contexts, formatted, placeholders, tests
+        ) = \
+        parser(
+            action, res, cmdname,
+            source, fmtinfo, trace, igc, test
+        )
 
-# Only save files to disk when not testing.
-if not test:
-    if formatting:
-        write(source, formatted)
-    else:
-        testpath = os.path.join(dirname, testname)
-        commandpath = os.path.join(dirname, savename)
-        commandconfigpath = os.path.join(dirname, saveconfigname)
-        placeholderspaths = os.path.join(dirname, "placeholders")
+        testname = cmdname + ".tests.sh"
+        savename = cmdname + ".acdef"
+        saveconfigname = "." + cmdname + ".config.acdef"
 
-        Path(dirname).mkdir(parents=True, exist_ok=True)
-        write(commandpath, acdef + keywords + filedirs + contexts)
-        write(commandconfigpath, config)
+        # Only save files to disk when not testing.
+        if not test:
+            if formatting:
+                write(source, formatted)
+            else:
+                testpath = os.path.join(dirname, testname)
+                commandpath = os.path.join(dirname, savename)
+                commandconfigpath = os.path.join(dirname, saveconfigname)
+                placeholderspaths = os.path.join(dirname, "placeholders")
 
-        # Save test file if tests were provided.
-        if tests:
-            write(testpath, tests) # [https://forum.nim-lang.org/t/5270]
-            # [https://stackoverflow.com/a/7228338]
-            os.chmod(testpath, 0o775) # 775 permissions
+                Path(dirname).mkdir(parents=True, exist_ok=True)
+                write(commandpath, acdef + keywords + filedirs + contexts)
+                write(commandconfigpath, config)
 
-        # Create placeholder files if object is populated.
-        # placeholders = placeholders
-        if placeholders:
-            Path(placeholderspaths).mkdir(parents=True, exist_ok=True)
+                # Save test file if tests were provided.
+                if tests:
+                    write(testpath, tests) # [https://forum.nim-lang.org/t/5270]
+                    # [https://stackoverflow.com/a/7228338]
+                    os.chmod(testpath, 0o775) # 775 permissions
 
-            for key in placeholders:
-                p = placeholderspaths + os.path.sep + key
-                write(p, placeholders[key])
+                # Create placeholder files if object is populated.
+                # placeholders = placeholders
+                if placeholders:
+                    Path(placeholderspaths).mkdir(parents=True, exist_ok=True)
 
-if print_:
-    if not formatting:
-        if acdef:
-            print("[" + chalk(cmdname + ".acdef", "bold") + "]\n")
-            print(acdef + keywords + filedirs + contexts)
-            if not config: print("")
-        if config:
-            msg = "\n[" + chalk("." + cmdname + ".config.acdef", "bold") + "]\n"
-            print(msg)
-            print(config + "\n")
-    else: print(formatted)
+                    for key in placeholders:
+                        p = placeholderspaths + os.path.sep + key
+                        write(p, placeholders[key])
 
-# Test (--test) purposes.
-if test:
-    if not formatting:
-        if acdef:
-            print(acdef + keywords + filedirs + contexts)
-            if not config: print("")
-        if config:
-            if acdef: print("")
-            print(config)
-    else: print(formatted)
+        if print_:
+            if not formatting:
+                if acdef:
+                    print("[" + chalk(cmdname + ".acdef", "bold") + "]\n")
+                    print(acdef + keywords + filedirs + contexts)
+                    if not config: print("")
+                if config:
+                    msg = "\n[" + chalk("." + cmdname + ".config.acdef", "bold") + "]\n"
+                    print(msg)
+                    print(config + "\n")
+            else: print(formatted)
+
+        # Test (--test) purposes.
+        if test:
+            if not formatting:
+                if acdef:
+                    print(acdef + keywords + filedirs + contexts)
+                    if not config: print("")
+                if config:
+                    if acdef: print("")
+                    print(config)
+            else: print(formatted)
+
+main()

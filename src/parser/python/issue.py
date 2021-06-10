8d91e51a1663e43299eb95d76882e445c1fc8cda
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-06-09 19:57:53 -0700

    update: Capitalize warning/error header

--- src/parser/python/issue.py
+++ src/parser/python/issue.py
@@ -5,13 +5,13 @@ import sys
 
 class Issue:
     def warn(self, filename, line, col, message):
-        itype = "\033[33;1mwarn:\033[0m"
+        itype = "\033[33;1mWarning:\033[0m"
         fileinfo = f"\033[1m{filename}:{line}:{col}:\033[0m"
 
         print(f"{fileinfo} {itype} {message}")
 
     def error(self, filename, line, col, message):
-        itype = "\033[31;1merror:\033[0m"
+        itype = "\033[31;1mError:\033[0m"
         fileinfo = f"\033[1m{filename}:{line}:{col}:\033[0m"
 
         sys.exit(f"{fileinfo} {itype} {message}")

0dd2ac314962c6e1e03915c7bc14b992f1d0038f
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-06-10 17:35:23 -0700

    update: Add hint function to issue family functions

--- src/parser/python/issue.py
+++ src/parser/python/issue.py
@@ -4,6 +4,12 @@
 import sys
 
 class Issue:
+    def hint(self, filename, line, col, message):
+        itype = "\033[32;1mHint:\033[0m"
+        fileinfo = f"\033[1m{filename}({line}, {col})\033[0m"
+
+        print(f"{fileinfo} {itype} {message}")
+
     def warn(self, filename, line, col, message):
         itype = "\033[33;1mWarning:\033[0m"
         fileinfo = f"\033[1m{filename}:{line}:{col}:\033[0m"

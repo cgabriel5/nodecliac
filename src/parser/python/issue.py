1b82002068d8bdc669728bde712dd25a6060537f
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-06-10 17:36:13 -0700

    update: Use parenthesis for line info

--- src/parser/python/issue.py
+++ src/parser/python/issue.py
@@ -12,12 +12,12 @@ class Issue:
 
     def warn(self, filename, line, col, message):
         itype = "\033[33;1mWarning:\033[0m"
-        fileinfo = f"\033[1m{filename}:{line}:{col}:\033[0m"
+        fileinfo = f"\033[1m{filename}({line}, {col})\033[0m"
 
         print(f"{fileinfo} {itype} {message}")
 
     def error(self, filename, line, col, message):
         itype = "\033[31;1mError:\033[0m"
-        fileinfo = f"\033[1m{filename}:{line}:{col}:\033[0m"
+        fileinfo = f"\033[1m{filename}({line}, {col})\033[0m"
 
         sys.exit(f"{fileinfo} {itype} {message}")

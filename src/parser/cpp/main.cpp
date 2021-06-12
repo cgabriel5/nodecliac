0959afa8841454808e0cabd09ca301818eb12771
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-06-11 18:15:30 -0700

    chore: Document cli arg parser library

--- src/parser/cpp/main.cpp
+++ src/parser/cpp/main.cpp
@@ -1,4 +1,4 @@
-#include <cxxopts.hpp>
+#include <cxxopts.hpp> // [https://github.com/jarro2783/cxxopts]
 #include <iostream>
 using namespace std;
 
@@ -12,11 +12,16 @@ int main(int argc, char **argv) {
 		// [https://github.com/jarro2783/cxxopts/pull/105]
 		.allow_unrecognised_options()
 		.add_options()
-		("trace", "",  cxxopts::value<bool>()->default_value("false"))
-		("action", "", cxxopts::value<std::string>())
-		("indent", "", cxxopts::value<std::string>()->default_value("s:4"))
-		("source", "", cxxopts::value<std::string>()->default_value(""));
-    auto result = options.parse(argc, argv);
+			("igc", "",    cxxopts::value<bool>()->default_value("false"))
+			("test", "",   cxxopts::value<bool>()->default_value("false"))
+			("print", "",  cxxopts::value<bool>()->default_value("false"))
+			("trace", "",  cxxopts::value<bool>()->default_value("false"))
+			("action", "", cxxopts::value<std::string>())
+			("indent", "", cxxopts::value<std::string>()->default_value("s:4"))
+			("source", "", cxxopts::value<std::string>()->default_value(""))
+			("positional", "", cxxopts::value<std::vector<std::string>>());
+	options.parse_positional({"action", "positional"});
+	auto result = options.parse(argc, argv);
 
 	bool igc = result["igc"].as<bool>();
 	bool test = result["test"].as<bool>();

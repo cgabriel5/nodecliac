3e65f0ece4e3dbf01347835cebf85147b9494415
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-06-27 23:35:51 -0700

    update: [checkpoint] continued work on lexer.go port

--- src/parser/go/main/utils/lexer/lexer.go
+++ src/parser/go/main/utils/lexer/lexer.go
@@ -1,3 +1,76 @@
 package lexer
 
-func Tokenizer(text string) {}
+import (
+	"github.com/cgabriel5/compiler/utils/structs"
+)
+
+type Token = structs.Token
+type State = structs.State
+
+const C_NL = '\n'
+const C_DOT = '.'
+const C_TAB = '\t'
+const C_PIPE = '|'
+const C_SPACE = ' '
+const C_QMARK = '?'
+const C_HYPHEN = '-'
+const C_ESCAPE = '\\'
+const C_LPAREN = '('
+const C_RPAREN = ')'
+const C_LCURLY = '{'
+const C_RCURLY = '}'
+const C_LBRACE = '['
+const C_RBRACE = ']'
+const C_ATSIGN = '@'
+const C_ASTERISK = '*'
+const C_DOLLARSIGN = '$'
+const C_UNDERSCORE = '_'
+
+var BRCTOKENS = map[string]string{
+	"C_LPAREN": "tkBRC_LP",
+	"C_RPAREN": "tkBRC_RP",
+	"C_LCURLY": "tkBRC_LC",
+	"C_RCURLY": "tkBRC_RC",
+	"C_LBRACE": "tkBRC_LB",
+	"C_RBRACE": "tkBRC_RB",
+}
+
+var LINESTARTS = map[int]int{1: -1}
+
+var KEYWORDS = [4]string{"default", "context", "filedir", "exclude"}
+// Invalid command start-of-token chars.
+var XCSCOPES = [4]byte{C_ATSIGN, C_DOT, C_LCURLY, C_RCURLY}
+
+var c byte
+var dtids = map[int]int{}
+var ttids []int
+var tokens []Token
+var ttypes = map[int]string{}
+var token_count = 0
+var cmdscope = false
+var valuelist = false // Value list.
+var brcparens []int
+
+func Tokenizer(text string) {
+	l := len(text)
+
+	var S = State{
+		I: 0,
+		Line: 1,
+		Kind: "",
+		Start: -1,
+		End: -1,
+		Lines: [2]int{-1, -1},
+		Last: false,
+		List: false,
+	}
+
+	for S.I < l {
+		c := text[S.I]
+
+		// Add 'last' key on last iteration.
+		if (S.I == l - 1) { S.Last = true }
+
+		S.I += 1
+	}
+}
--- src/parser/go/main/utils/structs/structs.go
+++ src/parser/go/main/utils/structs/structs.go
@@ -1,5 +1,20 @@
 package structs
 
+type Token struct {
+	Kind, Str_rep string
+	Line, Start, End, Tid int
+	Lines [2]int // = {-1, -1};
+}
+
+type State struct {
+	I, Line, Start, End int
+	Kind string
+	Last, List bool
+	Lines [2]int
+}
+
+// -----------------------------------------------------------------------------
+
 // [https://www.callicoder.com/golang-basic-types-operators-type-conversion/]
 type TabData struct {
 	Ichar   byte

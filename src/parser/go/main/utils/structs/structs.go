d344541dbd96ce8ba8632e02b37a9c0e9a9ca3c6
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-06-29 19:35:34 -0700

    update: Port remaining structs

--- src/parser/go/main/utils/structs/structs.go
+++ src/parser/go/main/utils/structs/structs.go
@@ -3,7 +3,15 @@ package structs
 type Token struct {
 	Kind, Str_rep string
 	Line, Start, End, Tid int
-	Lines [2]int // = {-1, -1};
+	Lines [2]int // = {-1, -1}
+}
+
+type LexerResponse struct {
+	Tokens []Token
+	Ttypes map[int]string
+	Ttids []int
+	Dtids map[int]int
+	LINESTARTS map[int]int // {1, -1}
 }
 
 type State struct {
@@ -20,6 +28,42 @@ type TabData struct {
 	Ichar   byte
 	Iamount int
 }
+
+type Args struct {
+	Action, Source string
+	Fmt TabData
+	Trace, Igc, Test bool
+}
+
+type Warning struct {
+	filename, message string
+	line, column int
+}
+
+type StateParse struct {
+	Tid int // = -1
+	Filename string // = source
+	Text string // = text
+	LexerData LexerResponse
+	Args Args
+	Ubids []int
+	Excludes []string
+	warnings map[int][]Warning
+	warn_lines []int // set?
+	warn_lsort []int // set?
+}
+
+type Flag struct {
+	Tid int // -1
+	Alias int // -1
+	Boolean int // -1
+	Assignment int // -1
+	Multi int // -1
+	Union_ int // -1
+	Values [][]int
+}
+
+// -----------------------------------------------------------------------------
 
 type FileInfo struct {
 	Name, Dirname, Ext, Path string

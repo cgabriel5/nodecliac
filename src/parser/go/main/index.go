44309782d3cfd93d1a705c236329f27761f5c1d4
cgabriel5 <cgabriel5@users.noreply.github.com>
2021-07-01 13:36:30 -0700

    update: Port remaining index.go logic (file storing, test/print actions)

--- src/parser/go/main/index.go
+++ src/parser/go/main/index.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"fmt"
 	"github.com/cgabriel5/compiler/utils/args"
-	"github.com/cgabriel5/compiler/utils/debug"
 	"github.com/cgabriel5/compiler/utils/fs"
 	"github.com/cgabriel5/compiler/utils/slices"
 	"github.com/cgabriel5/compiler/utils/parser"
@@ -130,8 +129,76 @@ func main() {
 	// if err != nil { panic(err) }
 	res := string(data)
 
-	parser.Parser(action, res, cmdname, *source, fmtinfo, *trace, *igc, *test)
+	acdef, config, keywords, filedirs,
+	contexts, formatted, oPlaceholders, tests := parser.Parser(action, res,
+		cmdname, *source, fmtinfo, *trace, *igc, *test)
 
-	debug.X(res, dirname, cmdname, igc, test, print, trace)
+	testname := cmdname + ".tests.sh"
+	savename := cmdname + ".acdef"
+	saveconfigname := "." + cmdname + ".config.acdef"
+
+	// Only save files to disk when not testing.
+	if !(*test) {
+	    if formatting {
+	        fs.Write(*source, &formatted, -1)
+	    } else {
+	        testpath := filepath.Join(dirname, testname)
+	        commandpath := filepath.Join(dirname, savename)
+	        commandconfigpath := filepath.Join(dirname, saveconfigname)
+	        placeholderspaths := filepath.Join(dirname, "placeholders")
+
+			// [https://stackoverflow.com/a/28448702]
+	        os.MkdirAll(dirname, os.ModePerm)
+	        acdef_ := acdef + keywords + filedirs + contexts
+	        fs.Write(commandpath, &acdef_, -1)
+	        fs.Write(commandconfigpath, &config, -1)
+
+	        // Save test file if tests were provided.
+	        if len(tests) > 0 {
+	            fs.Write(testpath, &tests, 0o775)
+			}
+
+	        // Create placeholder files if object is populated.
+	        // placeholders = placeholders
+	        if len(oPlaceholders) > 0 {
+	            os.MkdirAll(placeholderspaths, os.ModePerm)
+
+	            for key, value := range oPlaceholders {
+	                p := placeholderspaths + string(os.PathSeparator) + key
+	                fs.Write(p, &value, -1)
+				}
+			}
+		}
+	}
+
+    if *print {
+        if !formatting {
+			bold := color.New(color.Bold).SprintFunc()
+            if len(acdef) > 0 {
+                fmt.Println("[" + bold(cmdname + ".acdef") + "]\n")
+                fmt.Println(acdef + keywords + filedirs + contexts)
+                if len(config) == 0 { fmt.Println("") }
+            }
+            if len(config) > 0 {
+                msg := "\n[" + bold("." + cmdname + ".config.acdef") + "]\n"
+                fmt.Println(msg)
+                fmt.Println(config)
+			}
+        } else { fmt.Println(formatted) }
+	}
+
+    // Test (--test) purposes.
+    if *test {
+        if !formatting {
+            if len(acdef) > 0 {
+                fmt.Println(acdef + keywords + filedirs + contexts)
+                if len(config) == 0 { fmt.Println("") }
+			}
+            if len(config) > 0 {
+                if len(acdef) > 0 { fmt.Println("") }
+                fmt.Println(config)
+			}
+        } else { fmt.Println(formatted) }
+	}
 
 }

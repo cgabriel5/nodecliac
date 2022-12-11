c1b1bb02d4a5daf58c819f55d9fa98bfe965ec09
cgabriel5 <cgabriel5@users.noreply.github.com>
2022-12-11 13:25:31 -0800

    refactor: Move bash binary path location finder logic to own file

--- tests/scripts/shebang.sh
+++ tests/scripts/shebang.sh
@@ -39,37 +39,15 @@ __filepath="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 # ---------------------------------------------------------------------- IMPORTS
 
 . "$__filepath/common.sh"
+. "$__filepath/bashbin.sh"
 
 # ------------------------------------------------------------------------- VARS
 
-# Get list of valid Bash login shells: [https://stackoverflow.com/a/58270646]
-list=$(grep "/bin/bash$" <<< cat /etc/shells)
-
-# Split lines into array.
-# readarray -t shells <<< "$list" # [https://stackoverflow.com/a/19772067]
-IFS=$'\n' read -rd '' -a shells <<< "$list"
-
 ROOTDIR=$(chipdir "$__filepath" 2) # Get the project's root directory.
-
-valid_bash="" # Path to valid 4.3+ version of path.
-
-# Loop over shell paths.
-for ((i=${#shells[@]}-1; i>=0; i--)); do # [https://unix.stackexchange.com/a/27400]
-	path="${shells[$i]}"
-
-	# [https://stackoverflow.com/a/9450628]
-	version=$(perl -ne 'if (/ version ([\.\d]{3,})/) { print "$1"; }' <<< "$("$path" --version)")
-	# /usr/local/bin/bash -c 'echo "${BASH_VERSINFO:-0}"'
-	# /bin/bash -c 'echo "${BASH_VERSINFO:-0}"'
-
-	# Validate version number (must be >= 4.3).
-	if [[ $(perl -ne 'print 1 if $_ =~ /^([4-9]\.([3-9])|[5-9]\.\d).*$/' <<< "$version") ]]; then
-		valid_bash="$path"; break # Set valid Bash path and exit loop.
-	fi
-done
+bashpath="$(bashpath)"
 
 # If a valid Bash path exists reset all Bash shebangs.
-if [[ -n "$valid_bash" ]]; then
+if [[ -n "$bashpath" ]]; then
 	# [https://stackoverflow.com/a/15736463]
 	# [https://askubuntu.com/a/318211]
 	# [https://askubuntu.com/a/749708]
@@ -111,9 +89,9 @@ if [[ -n "$valid_bash" ]]; then
 	# [https://www.perlmonks.org/?node_id=151267]
 	# [https://stackoverflow.com/a/28035653]
 	# [https://gist.github.com/joyrexus/7328094]
-	# perl -i -ne "if (\$. == 1 and /^#!\/.*\/bash$/) { print \"#!$valid_bash\\n\" } else { print \$_ }; $. = 0 if eof" $files
+	# perl -i -ne "if (\$. == 1 and /^#!\/.*\/bash$/) { print \"#!$bashpath\\n\" } else { print \$_ }; $. = 0 if eof" $files
 	if [[ -n "$files" ]]; then
-		perl -i -ne "if (\$. == 1 and /^#!\/.*\/bash$/) { print \"#!$valid_bash\\n\" } else { print \$_ }; $. = 0 if eof" $files
+		perl -i -ne "if (\$. == 1 and /^#!\/.*\/bash$/) { print \"#!$bashpath\\n\" } else { print \$_ }; $. = 0 if eof" $files
 	fi
 
 	# GNU grep method (different output in macOS):
